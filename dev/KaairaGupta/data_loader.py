abacb51338b91059ee000a6e6a36e6ebd7e14786
Kaaira Gupta <kgupta@es.iitr.ac.in>
2020-03-08 01:53:35 +0530

    comparing different models using undersampling

--- dev/KaairaGupta/data_loader.py
+++ dev/KaairaGupta/data_loader.py
@@ -4,23 +4,34 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
-def load_split_preprocessed_data():
+
+
+def load_split_preprocessed_data(i):
+
     data = pd.read_csv("../../datasets/winequality.csv")
-    
+    Count_Fraud_transacation = len(data["recommend"]==True) 
+    fraud_indices= np.array((data.recommend==True).index)
+    normal_indices = np.array((data.recommend==False).index)
+
+    undersample_data = undersample(normal_indices,fraud_indices,i,Count_Fraud_transacation,data)
+
     # since data is skewed, we add epsilon to data and take its logarithm
-    data[['fixed acidity','volatile acidity','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','sulphates','alcohol','quality']] = np.add(data[['fixed acidity','volatile acidity','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','sulphates','alcohol','quality']],0.00000001)
-    data[['fixed acidity','volatile acidity','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol','quality']] = np.log(data[['fixed acidity','volatile acidity','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol','quality']])
+    undersample_data[['fixed acidity','volatile acidity','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','sulphates','alcohol','quality']] = np.add(undersample_data[['fixed acidity','volatile acidity','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','sulphates','alcohol','quality']],0.00000001)
+    undersample_data[['fixed acidity','volatile acidity','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol','quality']] = np.log(undersample_data[['fixed acidity','volatile acidity','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol','quality']])
+    undersample_data.drop(["quality"],axis=1,inplace=True)
+    x_features= undersample_data.drop(["recommend"],axis=1,inplace=False)
+    x_labels=undersample_data["recommend"]
 
-    sc = StandardScaler()
-    train = data.drop('recommend',axis = 1)
-    train = train.drop('quality', axis =1)
-    X_train, X_test, y_train, y_test = train_test_split(train, data.recommend, test_size=0.3,random_state=109)
-    X_train = sc.fit_transform(X_train)
-    X_test = sc.transform(X_test)
-    lda = LDA()
-    X_train = lda.fit_transform(X_train, y_train)
-    X_test = lda.transform(X_test)
+    x_features_train,x_features_test,x_labels_train,x_labels_test = train_test_split(x_features,x_labels,test_size=0.3)
+    print("hi")
 
-    return X_train, X_test, y_train, y_test
+    return(x_features_train,x_features_test,x_labels_train,x_labels_test)
+
+#undersampling data
+def undersample(normal_indices,fraud_indices,times, Count_Fraud_transacation,data):#times denote the normal data = times*fraud data
+    Normal_indices_undersample = np.array(np.random.choice(normal_indices,(times*Count_Fraud_transacation),replace=True))
+    undersample_data= np.concatenate([fraud_indices,Normal_indices_undersample])
+    undersample_data = data.iloc[undersample_data,:]
+    return(undersample_data)
     
 
