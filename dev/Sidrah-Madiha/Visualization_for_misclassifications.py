24ef0c78ff0375cebb7ba05bfa26074c2426a964
SMS <sidrahsms@yahoo.com>
2020-03-10 19:32:54 +0500

    fixes # 7

--- dev/Sidrah-Madiha/Visualization_for_misclassifications.py
+++ dev/Sidrah-Madiha/Visualization_for_misclassifications.py
@@ -1,17 +1,22 @@
 import pandas as pd
+import allcustommodules as sidra
+import matplotlib.pyplot as plt
+import numpy as np
 
 def missclassified_data_category_frquency(y_test, y_pred):
     ''' This function plots frequency of missclassified points against the incorrect categories they were predicted for
     parameters: 
     y_test: reverse factorized test values
     y_pred: reverse factorized predicted values '''
+    cm = sidra.create_confusion_matrix(y_test, y_pred)
+    for label in cm.columns:
+        cm.at[label,label] =0
     
-    missclassified = []
-    for pred,test in zip(y_pred, y_test): # assuming that y_test and y_pred is aleady untokenized, if not call untokenize_test_predict_data function
-        if pred != test:
-            missclassified.append(pred)
-    frequency_df =pd.DataFrame(missclassified)
-    frequency_df[0].value_counts().plot(kind='barh')
+    ax = cm.plot(kind='bar',  title='Predicted Class Error',  stacked=True)
+    ax.locator_params(axis='y',integer=True)
+    ax.set_xlabel('Classes/Categories')
+    ax.set_ylabel('Number of Incorrectly Predicted Class')
+    plt.show() 
     
 def untokenize_test_predict_data(definition, y_test, y_pred):
     ''' this function can be used to reverse factor test and predicted values before using 'missclassified_data_category_frquency' function
