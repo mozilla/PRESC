fef742fd776458e20636e596cd9c6378c4ac1f29
Addi-11 <saddi.5131@gmail.com>
2020-03-11 17:14:03 +0530

    fixes issue3

--- dev/Addi-11/data_split_examine.py
+++ dev/Addi-11/data_split_examine.py
@@ -8,27 +8,26 @@ import pandas as pd
 from IPython.display import HTML
 from pylab import *
 
-""" for now KNeighbors will be used as it gave the highest accuracy """
-model = Classifier()
-
-test_sizes = np.arange(0.0001,1,0.05)
-columns = ['Training data','Testing Data','Accuracy', 'Precision', 'Recall', 'F1_score']
+test_sizes = np.arange(0.005,1,0.05)
+columns = ['Training data','Testing Data','Accuracy %', 'Precision', 'Recall', 'F1_score']
 df = pd.DataFrame(columns = columns)
 
 def data_split_examine():
+    model = Classifier()
     for index in range(len(test_sizes)):
         X_train, X_test, y_train, y_test = train_test_split_data(test_sizes[index])
         classifier = model.KNeighbors(X_train, y_train)
-        accuracy, precision, recall, f_score, y = evaluate(classifier, X_test, y_test)
+        accuracy, precision, recall, f_score, _ = evaluate(classifier, X_test, y_test)
         train = round((1-test_sizes[index])*100)
         test = round(test_sizes[index]*100)
-        df.loc[index+1] = [train, test, accuracy, precision, recall, f_score]
+        df.loc[index+1] = [train, test, accuracy*100, precision, recall, f_score]
 
     display(df)
         
 def visualise_split():
     fig,axes = plt.subplots()
-    axes.set_xlabel("Accuracy")
-    axes.set_ylabel("Test Data Size")
+    axes.set_xlabel("Test Data Size")
+    axes.set_ylabel("Accuracy %")
+    axes.set_ylim([50,100])
     axes.set_title("Relation btw accuracy and test data size")
-    disp = axes.plot(test_sizes, df.Accuracy)
+    disp = axes.plot(df['Testing Data'], df['Accuracy %'])
