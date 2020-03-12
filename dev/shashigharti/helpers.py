0374650c0007bad79d0b72f669bac00779f62b17
Shashi Gharti <shashi.gharti@gmail.com>
2020-03-12 17:14:16 +0545

    Added docblock for functions

--- dev/shashigharti/helpers.py
+++ dev/shashigharti/helpers.py
@@ -5,6 +5,17 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import GridSearchCV 
 
 def train_svm(X, y, ratio = 0.3):
+    """ This function takes train data, labels as input and trains the SVM model.
+
+    Args:
+        X: Training Data(features columns).
+        y: Labels for each row
+        ratio: (optional) Split ratio for train and test data
+
+    Returns:
+        tuple: y_test, y_pred values
+    """
+    
     ## Split the train and test data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = ratio, random_state = 1)
     
@@ -13,9 +24,25 @@ def train_svm(X, y, ratio = 0.3):
     model_svc.fit(X_train,y_train)
     y_pred= model_svc.predict(X_test)
     
+    
     return y_test, y_pred
     
 def train_svm_with_hyperparameter_tuning(X, y, param_grid, ratio = 0.3):
+    """ This function takes train data, labels as input and trains the SVM model.
+
+    The function uses GridSearchCV for hyperparameter tuning for SVM
+    
+    Args:
+        X: Training Data(features columns).
+        y: Labels for each row
+        param_grid (json): params for GridSearchCV (c, gamma, kernel)
+        ratio: (optional) Split ratio for train and test data
+
+    Returns:
+        tuple: y_test, y_pred values
+
+    """
+    
     ## Split the train and test data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = ratio, random_state = 1)
 
@@ -26,6 +53,17 @@ def train_svm_with_hyperparameter_tuning(X, y, param_grid, ratio = 0.3):
     return y_test, y_pred
     
 def print_results(y_test, y_pred):
+    """ This function takes y_test and y_pred values and print the results
+    
+    Args:
+        y_test: Training Data(features columns).
+        y_pred: Labels for each row
+
+    Returns:
+        None
+
+    """
+    
     # Print Model Accuracy
     print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
     print(confusion_matrix(y_test, y_pred))
