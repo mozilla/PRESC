e237d47a16e5b084dc2c8ebcc665f2553ccf1556
Shiza Azam <shizaazam6@gmail.com>
2020-03-12 07:51:39 -0700

    change categorical data into numerical data

--- dev/shiza16/modules.py
+++ dev/shiza16/modules.py
@@ -13,7 +13,6 @@ def dataset_statistics(data):
     
     """ Data Features and Labels"""
     
-    print("DATASET STATISTICS: \n")
     print("Shape of the dataset: ",data.shape)
 
     print("\nFeatures of the dataset are: ",list(data.keys()))
@@ -31,17 +30,20 @@ def data_visualization(dataa):
     Vorder = dataa['Class'].value_counts().index
     sns.countplot(data = dataa, x = 'Class', color = base_color , order = Vorder)
     
+    print("\n")
     plt.figure(figsize = (25,15))
-    print("Correlation Analysis.")
     sns.heatmap(dataa.corr() ,annot = True , linewidths=.5)
+    print("Correlation Analysis.")
     plt.show()
 
     
 def splitting_train_test_data(data):
     
     """ Data is splitted into 30:70 for training and testing"""
-    X = data.drop(['Class'] , axis = 1)
-    y  = data['Class']
+    #X = data.drop(['Class'] , axis = 1)
+    #y  = data['Class']
+    X = data.drop(['Class', 'bus' , 'opel' , 'saab' , 'van'], axis = 1)
+    y = data[['bus' , 'opel' , 'saab' , 'van']]
     return train_test_split( X, y, test_size=0.3 , random_state=45)
 
 
@@ -74,24 +76,23 @@ def test_classifier(classifier , X_test):
     
 def model_confusion_matrix(y_test , y_predict , dataa):    
     
-    """ Confusion Matrix """
+    """ Drawing Confusion Matrix """
     
+    print("\n")
     plt.figure(figsize = (8,8))
     target_label = dataa['Class'].unique()
-    plt.xlabel('Real Vehicle Category')
-    plt.ylabel('Predicted Vehicle Category')
+    plt.xlabel('Actual Vehicle Labels Categoy')
+    plt.ylabel('Predicted Vehicle Labels Category')
 
     matrix = confusion_matrix(y_test, y_predict, labels = target_label)
     cmatrix = pd.DataFrame(matrix, index=target_label, columns=target_label)
-    sns.heatmap(cmatrix, annot=True, fmt='d')
-    
-    
+    sns.heatmap(cmatrix, annot=True , linewidths=.5)
     plt.show()
 
 def model_classification_report(y_test, y_predict):
     """  Model Classification report for Precision , Recall and F1-Score """
     
-    print("\n DataSet Report: ")
+    print("\nDataSet Report: ")
     print(classification_report(y_test, y_predict))
     
     
