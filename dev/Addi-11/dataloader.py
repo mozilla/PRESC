62edf5c003ca3c4010e6c521de19f454b007f7ec
Addi-11 <saddi.5131@gmail.com>
2020-03-14 01:56:45 +0530

    interprtation added

--- dev/Addi-11/dataloader.py
+++ dev/Addi-11/dataloader.py
@@ -1,5 +1,4 @@
-""" importing required dependencies """
-
+# importing required dependencies 
 from IPython.display import HTML 
 from tabulate import tabulate
 import pandas as pd
@@ -7,37 +6,68 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 
-""" read data file """
+# read data file
 address = "../../datasets/eeg.csv"
 df = pd.DataFrame(pd.read_csv(address))
-    
-    
-""" get some basic information about the dataset """
+	
 def data_info():
-    print("\nFirst 5 rows of datasets: ")
-    display(df.head(5))# first 5 rows of dataset
-    print("\nLast 5 rows of datasets: ")
-    display(df.tail(5)) # last 5 rows of dataset
-    print("\nInformation of each column: \n")
-    display(df.info()) # info of each column of dataset
-    print("\nChecking for null values: \n")
-    print(df.isnull().sum()) # sum of all null values in a dataset for preprocessing    
+	'''
+	Displays basic information about a dataset :
+		first 5 rows of dataset
+		last 5 rows of dataset
+		info of each column of dataset
+		description on the dataset
+		sum of all null values in a dataset for preprocessing
+	'''
+	print("\nFirst 5 rows of datasets: ")
+	display(df.head(5)) 
+	print("\nLast 5 rows of datasets: ")
+	display(df.tail(5))
+	print("\nDescription of each column: \n")
+	display(df.describe())   
+	print("\nInformation of each column: \n")
+	display(df.info()) 
+	print("\nChecking for null values: \n")
+	print(df.isnull().sum()) 
 
-""" Function specific to given dataset"""
+
 def data_visuals():
-    df.Class.unique()
-    sns.countplot(df.Class)
+	'''
+	Visualises basic infromation about the dataset.
+	This function is specific to the eeg dataset.
+	'''
+	df.Class.unique()
+	sns.countplot(df.Class)
 
-""" Getting X, y vlaues """
-def get_X_y():
-    y = df.Class
-    X = df.drop('Class',axis = 1)
-    return X, y
+def get_x_y():
+	y = df.Class
+	x = df.drop('Class',axis = 1)
+	return x,y
 
-""" splitting the data """
-def train_test_split_data(test_size):
-    X, y = get_X_y()
-    random_state = 40
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_size, random_state = random_state)
+def train_val_test_split_data(test_size):
+	'''
+	Splits the data into 3 portions, training, validation and testing.
+	
+	Parameters:
+		test_size : float-range(0,1), the ratio of test size to total size.
 
-    return X_train, X_test,y_train, y_test
+	Returns:
+		x_train : array-like, shape(n_train_samples, n_features)
+		x_val : of length n_validation_samples
+		x_test : array-like, shape(n_test_samples, n_features)
+		y_train : of length n_train_samples
+		y_val : of length n_validation_samples
+		y_test : of length n_test_samples
+	'''
+	
+	x, y = get_x_y()
+	random_state = 40
+	
+	# dividing training set into training and testing segments
+	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = test_size, random_state = random_state)
+	
+	# further dividing training set into training and validation segments
+	val_size = 0.2 # set the validation size 0.2 of the training set
+	x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size= val_size, random_state = random_state)
+
+	return x_train, x_val, x_test,y_train, y_val, y_test
--- dev/Addi-11/evaluation.py
+++ dev/Addi-11/evaluation.py
@@ -1,4 +1,4 @@
-""" This file contain evaluation metrics for the model"""
+# This file contain evaluation metrics for the model
 
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score,confusion_matrix, plot_confusion_matrix,average_precision_score,precision_recall_curve, plot_precision_recall_curve, f1_score, precision_score, recall_score
@@ -7,17 +7,26 @@ import pandas as pd
 from sklearn.utils import class_weight
 
 
-def evaluate(classifier, X_test, y_test):
-    y_score = classifier.predict(X_test)
-    accuracy = accuracy_score(y_test, y_score)
-    f_score = f1_score(y_test, y_score)
-    precision = precision_score(y_test, y_score)
-    recall = recall_score(y_test, y_score, zero_division=1)
+def evaluate(classifier, x_val, y_val):
+    '''
+    This function predicts the values for a trained model and calculates the evaluation metrics accuracy, precision, recall, f_score on the validation set. 
+    Parameters :
+        classifier : trained classification model
+        x_val : array-like, shape( n_samples, n_features)
+        y_val : array-like, of length n_samples
 
-    # print("Accuracy : ",accuracy)
-    # print("Precision: ", precision)
-    # print("Recall: ", recall)
-    # print("F1 score : ",f_score)
+    Returns :
+        accuracy : float
+        precision : float
+        recall : float
+        f_score : float
+        y_score : array-like of length n_features, 
+    '''
+    y_score = classifier.predict(x_val)
+    accuracy = accuracy_score(y_val, y_score)
+    f_score = f1_score(y_val, y_score)
+    precision = precision_score(y_val, y_score)
+    recall = recall_score(y_val, y_score)
 
     return accuracy, precision, recall, f_score, y_score
 
