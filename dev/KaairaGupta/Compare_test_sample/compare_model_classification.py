71df092ca7ed513c40c35b65c37e299dcae2d36f
Kaaira Gupta <kgupta@es.iitr.ac.in>
2020-03-13 22:10:10 +0530

    reformat compare_model_classification.py using black

--- dev/KaairaGupta/Compare_test_sample/compare_model_classification.py
+++ dev/KaairaGupta/Compare_test_sample/compare_model_classification.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def compare_model_classification(model1_res, model2_res, y_test):
     """
         This function plot visualization to compare predicted class probabilities across two binary-classifier models.
@@ -10,33 +11,38 @@ def compare_model_classification(model1_res, model2_res, y_test):
             y_test: The actual value of y (class) (either 0 or 1).
         Output: Two visualisations (one for each class, 0 and 1) for each model.
     """
-    
+
     # table t contains probablity values given by both models for each model
-    t = [[],[]]
-    
+    t = [[], []]
+
     for i in range(len(model1_res)):
         y = y_test[i]
-        temp = [min(model1_res[i][y], round(model1_res[i][y])), min(model2_res[i][y], round(model2_res[i][y]))]
-        if not (temp[0]==0 and temp[1]==0):
+        temp = [
+            min(model1_res[i][y], round(model1_res[i][y])),
+            min(model2_res[i][y], round(model2_res[i][y])),
+        ]
+        if not (temp[0] == 0 and temp[1] == 0):
             t[y].append(temp)
 
     t_trans = [np.array(t[0]).transpose(), np.array(t[1]).transpose()]
-    
+
     for i in range(2):
         fig, ax = plt.subplots()
-        
+
         # x used as index to show performance at various test examples
         x = np.arange(len(t[i]))
         plt.xticks(x)
-        
-        plt.bar(x-0.125, t_trans[i][:][0], color='y', width=0.25, label="Model 1")
-        plt.bar(x-0.125+0.25, t_trans[i][:][1], color='g', width=0.25, label="Model 2")
 
-        ax.set_xlabel('Index: Test Examples')
-        ax.set_ylabel('Probablity for correct class (y={}) prediction'.format(i))
+        plt.bar(x - 0.125, t_trans[i][:][0], color="y", width=0.25, label="Model 1")
+        plt.bar(
+            x - 0.125 + 0.25, t_trans[i][:][1], color="g", width=0.25, label="Model 2"
+        )
+
+        ax.set_xlabel("Index: Test Examples")
+        ax.set_ylabel("Probablity for correct class (y={}) prediction".format(i))
 
         title = "Performace for y = {} for two models.".format(i)
-        fig.suptitle(title,fontweight="bold")
+        fig.suptitle(title, fontweight="bold")
 
         plt.legend()
         plt.show()
