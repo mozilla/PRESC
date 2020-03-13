d90069188b4cf1c4ab3a52d35ae357dc82af43ce
Shashi Gharti <shashi.gharti@gmail.com>
2020-03-13 23:56:42 +0545

    updated code for cross_validation function

--- dev/shashigharti/issue4_helper.py
+++ dev/shashigharti/issue4_helper.py
@@ -1,24 +1,23 @@
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
 from sklearn import metrics
 import pandas as pd
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-
+from sklearn.model_selection import StratifiedKFold
 
 def cross_validation(estimator, X, y):
-    """ This function takes estimator and data as input
-
+    """ This function takes estimator and data as input and does cross-validation tests.
     Args:
-        estimator: 
-        X: Training Data(features columns).
+        estimator: Model to be trained
+        X: Training Data(features columns)
         y: Labels for each row
-        
     Returns:
-        list: list with test results
+        dataframe: list of test-train split results
     """  
     result_lst = list()       
     
-    
-    return result_lst
+
+    # Convert the list to dataframe
+    result_df = pd.DataFrame(result_lst, columns = ['Ratio(Train Data)', 'Ratio(Test Data)', 'Accuracy', 'F1-Score'])
+        
+    print(result_df)
+    return result_df
