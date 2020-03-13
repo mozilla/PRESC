63cd20c87deed6a5017b2bba46d429d7cddac0fc
Shashi Gharti <shashi.gharti@gmail.com>
2020-03-13 21:55:51 +0545

    Updated function for test-train split experiment
    1. Updated test_train_split function
    2. Updated function call in python notebook

--- dev/shashigharti/issue3_helper.py
+++ dev/shashigharti/issue3_helper.py
@@ -1,9 +1,13 @@
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
-def train_svm_with_hyperparameter_tuning(X, y, param_grid, ratio=0.3):
+def test_train_split(estimator, X, y):
     """ This function takes train data, labels as input and trains the SVM model.
     The function uses GridSearchCV for hyperparameter tuning for SVM
 
@@ -14,12 +18,24 @@ def train_svm_with_hyperparameter_tuning(X, y, param_grid, ratio=0.3):
         ratio: (optional) Split ratio for train and test data
     Returns:
         tuple: y_test, y_pred values
-    """
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=ratio, random_state=1)
-
-    svm_grid = GridSearchCV(SVC(), param_grid, refit=True, verbose=3)
-    svm_grid.fit(X_train, y_train)
-    y_pred = svm_grid.predict(X_test)
-
-    return y_test, y_pred
+    """  
+    result_lst = list()       
+    
+    for i in range(10, 100, 5):
+        
+        # Calculate Test/Train Ratio
+        test_ratio = round(i/100 , 2)
+        train_ratio = round(1 - test_ratio, 2)
+        
+        # Split the data based on the calculated test ratio
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio, random_state=1)
+        
+        estimator.fit(X_train, y_train)
+        y_pred = estimator.predict(X_test)
+        result_lst.append([train_ratio, test_ratio, metrics.accuracy_score(y_test, y_pred), metrics.f1_score(y_test, y_pred, average='weighted')])
+        
+        # Convert the list to dataframe
+        result_df = pd.DataFrame(result_lst, columns = ['Ratio(Train Data)', 'Ratio(Test Data)', 'Accuracy', 'F1-Score']) 
+        
+    print(result_df)
+    return result_df
