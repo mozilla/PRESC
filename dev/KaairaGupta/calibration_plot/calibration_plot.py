515b3f4642af5a682ea341eb3cda666a8db46faf
Kaaira Gupta <kgupta@es.iitr.ac.in>
2020-03-14 17:54:30 +0530

    reformatted calibration_plot using black

--- dev/KaairaGupta/calibration_plot/calibration_plot.py
+++ dev/KaairaGupta/calibration_plot/calibration_plot.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 import matplotlib.transforms as mtransforms
 
+
 def calibration_plot(clf_list, clf_names, test_data, labels_test, n_bins):
     """
         Input:
@@ -18,21 +19,23 @@ def calibration_plot(clf_list, clf_names, test_data, labels_test, n_bins):
 
     for clf in clf_list:
         pred_p = clf.predict_proba(test_data)
-        y, x = calib_c(labels_test, pred_p[:,1], n_bins=10)
-        cc_list.append([x,y])
+        y, x = calib_c(labels_test, pred_p[:, 1], n_bins=10)
+        cc_list.append([x, y])
 
     fig, ax = plt.subplots()
 
     for i in range(len(cc_list)):
-        plt.plot(cc_list[i][0],cc_list[i][1], marker='o', linewidth=1, label=clf_names[i])
+        plt.plot(
+            cc_list[i][0], cc_list[i][1], marker="o", linewidth=1, label=clf_names[i]
+        )
 
     # reference line, legends, and axis labels
-    line = mlines.Line2D([0, 1], [0, 1], color='black')
+    line = mlines.Line2D([0, 1], [0, 1], color="black")
     transform = ax.transAxes
     line.set_transform(transform)
     ax.add_line(line)
-    fig.suptitle('Calibration plots')
-    ax.set_xlabel('Predicted probability')
-    ax.set_ylabel('True probability in each bin')
+    fig.suptitle("Calibration plots")
+    ax.set_xlabel("Predicted probability")
+    ax.set_ylabel("True probability in each bin")
     plt.legend()
     plt.show()
