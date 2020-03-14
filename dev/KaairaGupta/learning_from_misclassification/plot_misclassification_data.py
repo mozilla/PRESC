5d34aff631b6920e5c0384edab44df239e3cc995
Kaaira Gupta <kgupta@es.iitr.ac.in>
2020-03-14 14:10:05 +0530

    completed first attemp at #63

--- dev/KaairaGupta/learning_from_misclassification/plot_misclassification_data.py
+++ dev/KaairaGupta/learning_from_misclassification/plot_misclassification_data.py
@@ -2,19 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_misclassified_probablities(model_res, correct_y):
+def plot_misclassified_probablities(model_res, class_labels, correct_y_labels):
     """
         Input:
             model_res: Model result (n X k) matrix where n = number of test_examples, k = number of classes.
                 Each row has probablity values predicted by the model for each of k classes.
-            correct_y: Array of n values with each value between [1,k], k = number of classes.
+            class_lables: Labels for classes (1,2,3,...k) in order according to model_res.
+            correct_y: Array of n values with each value as label for classes.
         Output:
             Plot showing classification probablities for misclassified data points.
     """
 
     # convert both input to numpy
     model_res = np.array(model_res)
-    correct_y = np.array(correct_y)
+    correct_y_labels = np.array(correct_y_labels)
+
+    # create array correct_y to map correct_y_labels to corresponding index of class_labels
+    correct_y = []
+    for label in correct_y_labels:
+        correct_y.append((np.where(class_labels == label))[0][0])
 
     # k = number of classes
     k = len(model_res[0])
@@ -24,21 +30,22 @@ def plot_misclassified_probablities(model_res, correct_y):
 
     # multi-class classification (predicted class is one with highest probablity)
     if k > 2:
-        for res_i in len(model_res):
+        for res_i in range(len(model_res)):
             res = model_res[res_i]
             max_i = np.where(res == np.amax(res))
-            if (len(max_i) > 1) or (max_i[0] != correct_y[res_i] - 1):
+            if (len(max_i) > 1) or (max_i[0][0] != correct_y[res_i]):
                 index_misclassification.append(res_i)
     elif k == 2:
-        for res_i in len(model_res):
+        for res_i in range(len(model_res)):
             res = model_res[res_i]
-            if not (res[correct_y[res_i] - 1] > 0.5):
+            if not (res[correct_y[res_i]] > 0.5):
                 index_misclassification.append(res_i)
     else:
         print("Invalid input.\nNumber of classes must be greater than pr equal to 2.")
         return
 
-    print("Misclassified test cases: " + index_misclassification)
+    # misclassification indices
+    print("Number of misclassified datapoints: {}".format(len(index_misclassification)))
 
     # plotting data
     fig, ax = plt.subplots()
@@ -50,11 +57,14 @@ def plot_misclassified_probablities(model_res, correct_y):
     x_shift = x - ((k - 1) * 0.25) - 0.125
 
     for i in range(k):
+        label = "Class {}".format(class_labels[i])
+        misc_probabs = []
+
+        for j in range(len(index_misclassification)):
+            misc_probabs.append(model_res[index_misclassification[j]][i])
+
         plt.bar(
-            x_shift + (0.25 * i),
-            model_res[:][i],
-            width=0.25,
-            label="Class {}".format(k + 1),
+            x_shift + (0.25 * i), misc_probabs, width=0.25, label=label,
         )
 
     ax.set_xlabel("Indices of misclassifications in Model Result.")
