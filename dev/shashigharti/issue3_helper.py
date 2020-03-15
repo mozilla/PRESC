bb6aabee5c24061a1546fbf054f63d3f7971ddef
Shashi Gharti <shashi.gharti@gmail.com>
2020-03-15 22:00:38 +0545

    Added 3 different functions to handle outliers

--- dev/shashigharti/issue3_helper.py
+++ dev/shashigharti/issue3_helper.py
@@ -1,13 +1,9 @@
-from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
 from sklearn import metrics
 import pandas as pd
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
-def test_train_split(estimator, X, y):
+def test_train_split(estimator, X, y, scaler = None):
     """ This function takes estimator and data as input and does test-train split test in multiple passes.
 
     Args:
@@ -29,6 +25,10 @@ def test_train_split(estimator, X, y):
         # Split the data based on the calculated test ratio
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio, random_state=1)
         
+        if scaler is not None:
+            X_train = scaler.fit_transform(X_train)
+            X_test = scaler.fit_transform(X_test)
+        
         estimator.fit(X_train, y_train)
         y_pred = estimator.predict(X_test)
         result_lst.append([train_ratio, test_ratio, metrics.accuracy_score(y_test, y_pred), metrics.f1_score(y_test, y_pred, average='weighted')])
@@ -37,4 +37,5 @@ def test_train_split(estimator, X, y):
         result_df = pd.DataFrame(result_lst, columns = ['Ratio(Train Data)', 'Ratio(Test Data)', 'Accuracy', 'F1-Score']) 
         
     print(result_df)
+    
     return result_df
