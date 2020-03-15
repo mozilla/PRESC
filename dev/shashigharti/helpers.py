a4f4d6d4508aa6a677d1009b25e56e3e5a4019f6
Shashi Gharti <shashi.gharti@gmail.com>
2020-03-15 23:08:49 +0545

    Added two conservative function for outlier removal

--- dev/shashigharti/helpers.py
+++ dev/shashigharti/helpers.py
@@ -3,8 +3,78 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import GridSearchCV 
+import pandas as pd
+
+def fix_outlier_with_mean(df, df_columns_with_outliers):   
+    """ This function takes data as input and replaces outliers of each columns by mean
     
-def train_svm(X, y, scaler = None, ratio = 0.2):
+    Args:
+        df: dataframe of all data
+    Returns:
+        data_df: processed dataframe
+        
+    """
+    data_df = df.copy()
+
+    # Fill null
+    data_df.fillna(data_df.mean(), inplace=True)
+
+    # Replace outliers with mean value. Mean is calculated with data rows excluding outliers
+    for i, column in enumerate(df_columns_with_outliers.columns):
+        data_df.loc[data_df[column] > df_columns_with_outliers[column][0], column] = data_df[data_df[column] <= df_columns_with_outliers[column][0]][column].mean()
+
+    data_df['Class'] = pd.Categorical(data_df['Class']).codes
+    
+    return data_df
+
+def fix_outlier_with_boundary_value(df, df_columns_with_outliers): 
+    """ This function takes data as input and replaces outliers > max value of each columns by max value
+    
+    Args:
+        df: dataframe of all data
+    Returns:
+        data_df: processed dataframe
+        
+    """
+    data_df = df.copy()
+
+    # Fill null
+    data_df.fillna(data_df.mean(), inplace=True)
+
+    # Replace outliers with max boundary value
+    for i, column in enumerate(df_columns_with_outliers.columns):
+        data_df.loc[data_df[column] > df_columns_with_outliers[column][0], column] = df_columns_with_outliers[column][0]
+        
+    data_df['Class'] = pd.Categorical(data_df['Class']).codes
+    
+    return data_df
+
+def remove_outliers(df, df_columns_with_outliers):
+    """ This function takes data as input and removes the outliers
+    
+    Args:
+        df: dataframe of all data
+    Returns:
+        data_df: processed dataframe
+        
+    """
+    data_df = df.copy()
+    
+    # Fill null
+    data_df.fillna(data_df.mean(), inplace=True)
+
+    # Remove outliers based on max value identified earlier from boxplot
+    for i, column in enumerate(df_columns_with_outliers.columns):
+        data_df = data_df[data_df[column] < df_columns_with_outliers[column][0]]
+
+    data_df['Class'] = pd.Categorical(data_df['Class']).codes
+    
+    # reset the index post cleaning the outliers
+    data_df = data_df.reset_index(drop=True)
+    
+    return data_df
+    
+def train_svm(X, y, scaler = None, ratio = 0.4):
     """ This function takes train data, labels as input and trains the SVM model.
     
     Args:
@@ -15,6 +85,7 @@ def train_svm(X, y, scaler = None, ratio = 0.2):
 
     Returns:
         tuple: y_test, y_pred values
+        
     """
     
     ## Split the train and test data
@@ -33,7 +104,7 @@ def train_svm(X, y, scaler = None, ratio = 0.2):
     
     return y_test, y_pred
     
-def train_svm_with_hyperparameter_tuning(X, y, param_grid, scaler = None, ratio = 0.3):
+def train_svm_with_hyperparameter_tuning(X, y, param_grid, scaler = None, ratio = 0.4):
     """ This function takes train data, labels as input and trains the SVM model.
 
     The function uses GridSearchCV for hyperparameter tuning for SVM
