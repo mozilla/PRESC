0ff68ca156d5236bc2d1df0b39e5c520a643fb04
Kaaira Gupta <kgupta@es.iitr.ac.in>
2020-03-15 11:28:13 +0530

    improve function code for better readability

--- dev/KaairaGupta/calibration_plot/calibration_plot.py
+++ dev/KaairaGupta/calibration_plot/calibration_plot.py
@@ -1,39 +1,62 @@
-from sklearn.calibration import calibration_curve as calib_c
+from sklearn.calibration import calibration_curve
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 import matplotlib.transforms as mtransforms
 
 
-def calibration_plot(clf_list, clf_names, test_data, labels_test, n_bins):
+def calibration_plot(
+    classifier_model_list, classifier_model_names, features_test, labels_test, n_bins
+):
     """
         Input:
-            clf_list: list of objects of a classifier which has method clf.predict_proba(test_data) with output y = list of predicted prbablities for two classes <np.array<np.array[2]>>
-            test_data: input for clf.predict(test_data)
-            test_data_labels: correct labels for test data
-            n_bins: number of bins
-            clf_names: list of names clf in clf_list
-        Output: Plots a calibartion plot displaying the observed occurrence rates vs the bin midpoints for each mode
+            classifier_model_list: List of objects of binary classifier models which have method 
+                classifier.predict_proba(features_test) with output y = list of predicted prbablities 
+                for two classes (binary classification) <np.array<np.array[2]>>.
+            
+            classifier_model_names: List of names (string array) of classifiers in classifier_model_list.
+            
+            features_test: Input features for classification, classifier.predict_proba(features_test).
+            
+            labels_test: Correct labels for test data (features_test).
+            
+            n_bins: Number of bins for calibration plot.
+        
+        Output: Plots a calibartion plot displaying the observed occurrence rates vs the bin 
+            midpoints for each mode
     """
 
-    cc_list = []
+    # List of values of y for each x for calibration curves of each model.
+    calibration_curve_x_y = []
 
-    for clf in clf_list:
-        pred_p = clf.predict_proba(test_data)
-        y, x = calib_c(labels_test, pred_p[:, 1], n_bins=10)
-        cc_list.append([x, y])
+    # Fill calibration_curves_x_y
+    for classifier in classifier_model_list:
+        predicted_probablities = classifier.predict_proba(features_test)
 
-    fig, ax = plt.subplots()
-
-    for i in range(len(cc_list)):
-        plt.plot(
-            cc_list[i][0], cc_list[i][1], marker="o", linewidth=1, label=clf_names[i]
+        y, x = calibration_curve(
+            labels_test, predicted_probablities[:, 1], n_bins=n_bins
         )
 
-    # reference line, legends, and axis labels
+        calibration_curve_x_y.append([x, y])
+
+    # Plot calibaration curve comparing different models in classifier_model_list.
+    fig, ax = plt.subplots()
+
+    for i in range(len(calibration_curve_x_y)):
+        plt.plot(
+            calibration_curve_x_y[i][0],
+            calibration_curve_x_y[i][1],
+            marker="o",
+            linewidth=1,
+            label=classifier_model_names[i],
+        )
+
+    # Reference line (y=x)
     line = mlines.Line2D([0, 1], [0, 1], color="black")
     transform = ax.transAxes
     line.set_transform(transform)
     ax.add_line(line)
+
+    # Title, legend, and axis labels
     fig.suptitle("Calibration plots")
     ax.set_xlabel("Predicted probability")
     ax.set_ylabel("True probability in each bin")
