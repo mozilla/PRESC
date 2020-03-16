0757ee79c0a9be188515938128546397d9c13514
shiza16 <shizaazam6@gmail.com>
2020-03-16 16:31:46 -0700

    added docstring and reformatted to python black

--- dev/shiza16/VisualizationForMisclassification.py
+++ dev/shiza16/VisualizationForMisclassification.py
@@ -1,24 +1,30 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import modules as modle
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import confusion_matrix
 
-def Missclasssification_visualization(y_test, y_predict,data):
-    
-    labels = data["Class_code"].unique()
+
+def Missclasssification_visualization(y_test, y_predict, data):
+    """  
+    Visualizing Bar graph for Missclassification, values
+    that are incorreclty predicted by a classifier.
+
+    """
+
+    target_label = data["Class_code"].unique()
     label = data["Class"].unique()
     cmatrix = pd.DataFrame(
-            data= modle.model_confusion_matrix(y_test, y_predict, data) , 
-            columns=label,
-            index=label
-            )
+        data=confusion_matrix(y_test, y_predict, labels=target_label),
+        columns=label,
+        index=label,
+    )
 
-    ##Because we only want to print missclassification, 
+    ##Because we only want to print missclassification,
     for label in cmatrix.columns:
-            cmatrix.at[label, label] = 0
+        cmatrix.at[label, label] = 0
 
-    graph = cmatrix.plot(kind="bar", title="Visualization of Missclassification", stacked=True)
+    graph = cmatrix.plot(
+        kind="bar", title="Visualization of Missclassification", stacked=True
+    )
     graph.set_xlabel("Class Labels")
     graph.set_ylabel("Missclassified Classes")
     plt.show()
