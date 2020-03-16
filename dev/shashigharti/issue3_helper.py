8d03143cd4543ec32166f053426b52716d48178b
Shashi Gharti <shashi.gharti@gmail.com>
2020-03-16 12:00:57 +0545

    Fixed transformation code
    1. Fixed Transformation Code
    2. Added warnings

--- dev/shashigharti/issue3_helper.py
+++ dev/shashigharti/issue3_helper.py
@@ -1,6 +1,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 import pandas as pd
+import warnings
 
 def fix_outlier_with_boundary_value(df, df_columns_with_outliers): 
     """ This function takes data as input and replaces outliers > max value of each columns by max value
@@ -35,6 +36,9 @@ def test_train_split(estimator, X, y, scaler = None):
     Returns:
         dataframe: list of test-train split results
     """  
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+    warnings.simplefilter(action='ignore', category=DeprecationWarning)
+    
     result_lst = list()       
     
     for i in range(10, 100, 5):
@@ -47,8 +51,9 @@ def test_train_split(estimator, X, y, scaler = None):
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio, random_state=1)
         
         if scaler is not None:
-            X_train = scaler.fit_transform(X_train)
-            X_test = scaler.fit_transform(X_test)
+            scaler.fit(X_train)
+            X_train = scaler.transform(X_train)
+            X_test = scaler.transform(X_test)
         
         estimator.fit(X_train, y_train)
         y_pred = estimator.predict(X_test)
