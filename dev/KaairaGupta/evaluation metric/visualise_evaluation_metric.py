8461ec25ee6db3a17a5dc096619f2bcf67ed2ea4
Kaaira Gupta <kgupta@es.iitr.ac.in>
2020-03-18 18:05:32 +0530

    reformat using black

--- dev/KaairaGupta/evaluation metric/visualise_evaluation_metric.py
+++ dev/KaairaGupta/evaluation metric/visualise_evaluation_metric.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def visualise_evaluation_metric(table):
 
     """
@@ -10,14 +11,14 @@ def visualise_evaluation_metric(table):
     """
 
     x = table[0]
-    
+
     _mean = np.empty(len(x))
     _min = np.empty(len(x))
     _max = np.empty(len(x))
     _std = np.empty(len(x))
-    
+
     table = table.transpose()
-    
+
     for i in range(len(table)):
         r = table[i]
 
@@ -25,11 +26,13 @@ def visualise_evaluation_metric(table):
         _min[i] = np.min(r[1:])
         _max[i] = np.max(r[1:])
         _std[i] = np.std(r[1:])
-    
-    plt.plot(x, _mean, label='Mean', linewidth=2, color='g')
-    plt.plot(x, _min, label='Min',linewidth=1, color='y')
-    plt.plot(x, _max, label='Max',linewidth=1, color='r')
-    plt.fill_between(x, _mean-(_std/2), _mean+(_std/2), alpha=0.3, label='Standard Deviation')
-    
+
+    plt.plot(x, _mean, label="Mean", linewidth=2, color="g")
+    plt.plot(x, _min, label="Min", linewidth=1, color="y")
+    plt.plot(x, _max, label="Max", linewidth=1, color="r")
+    plt.fill_between(
+        x, _mean - (_std / 2), _mean + (_std / 2), alpha=0.3, label="Standard Deviation"
+    )
+
     plt.legend()
     plt.show()
