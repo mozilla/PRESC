c8c0334d52ba27c35cf5de9c3a07049030786bc6
Melius <elie.wanko@gmail.com>
2020-03-18 23:18:16 +0300

    Files reformated with 'Black'

--- dev/elie_wanko/modules/helpers.py
+++ dev/elie_wanko/modules/helpers.py
@@ -1,36 +1,54 @@
-#Libraries
+# Libraries
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def univ_bar(title=None, fig_w=10, fig_h=5, var_names=None, hue=None, palette=None, order=False, x_title=None, *, data, column):    
-    #Make a deep copy of dataframe
+def univ_bar(
+    title=None,
+    fig_w=10,
+    fig_h=5,
+    var_names=None,
+    hue=None,
+    palette=None,
+    order=False,
+    x_title=None,
+    *,
+    data,
+    column
+):
+    # Make a deep copy of dataframe
     df_clone = data.copy()
-    #Exacts distinct entries of an attricbute(comumn) and Sorts it into a list
+    # Exacts distinct entries of an attricbute(comumn) and Sorts it into a list
     sorted_list = sorted(set(df_clone[column]))
-    
-    #Extract and Replace categorical numerical values to Friendgly words(var_names)
+
+    # Extract and Replace categorical numerical values to Friendgly words(var_names)
     if var_names != None:
         df_clone[column] = df_clone[column].replace(sorted_list, var_names)
-    
-    #Plot bar graph
-    plt.figure(figsize=(fig_w, fig_h));
-    if order == True:
-        sns.countplot(data=df_clone, x=x_title, hue=hue, palette=palette, order=df_clone[column].value_counts().index);
-    else:
-        sns.countplot(data=df_clone, x=x_title, hue=hue, palette=palette);
-    # Set title
-    plt.title(title, fontsize=14);
 
-        
-def univ_hist(title=None, fig_w=18, fig_h=7, _hue=None, bins=10, *, data, column):    
-    #Make a deep copy of dataframe
+    # Plot bar graph
+    plt.figure(figsize=(fig_w, fig_h))
+    if order == True:
+        sns.countplot(
+            data=df_clone,
+            x=x_title,
+            hue=hue,
+            palette=palette,
+            order=df_clone[column].value_counts().index,
+        )
+    else:
+        sns.countplot(data=df_clone, x=x_title, hue=hue, palette=palette)
+    # Set title
+    plt.title(title, fontsize=14)
+
+
+def univ_hist(title=None, fig_w=18, fig_h=7, _hue=None, bins=10, *, data, column):
+    # Make a deep copy of dataframe
     df_clone = data.copy()
-    
-    #Plot hist graph
-    plt.figure(figsize=(fig_w, fig_h));
+
+    # Plot hist graph
+    plt.figure(figsize=(fig_w, fig_h))
     sns.distplot(df_clone[column], bins=bins)
     # Set title
-    plt.title(title, fontsize=14);
+    plt.title(title, fontsize=14)
--- dev/elie_wanko/modules/knn.py
+++ dev/elie_wanko/modules/knn.py
@@ -1,10 +1,11 @@
-#Imports
+# Imports
 from numpy import random, array
 from math import floor
 
 from sklearn.neighbors import KNeighborsRegressor
 
-def classfier(cut_off = 10, n_nearest = 5, threshold=None, *, data):
+
+def classfier(cut_off=10, n_nearest=5, threshold=None, *, data):
     """ Returns the predicted values computed from a simple majority vote of the nearest neighbors of each point.
     
     Keyword arguments:
@@ -13,7 +14,7 @@ def classfier(cut_off = 10, n_nearest = 5, threshold=None, *, data):
     n_nearest(int) -- nearest neighbors searches (default = 5)
     threshold(float, [0.0-1.0] -- at what probb will an observation default
     """
-    
+
     # Split training and testing attributes
     training_attributes = data.columns[:-1]
     testing_attribute = data.columns[-1]
@@ -22,7 +23,7 @@ def classfier(cut_off = 10, n_nearest = 5, threshold=None, *, data):
     random_indices = random.permutation(data.index)
 
     # Set a cutoff for how many items we want in the test set (in this case 1/10 of the items)
-    cut_off = floor(len(data)/cut_off)
+    cut_off = floor(len(data) / cut_off)
 
     # Generate the test and train set.
     test_data = data.loc[random_indices[1:cut_off]]
@@ -32,20 +33,19 @@ def classfier(cut_off = 10, n_nearest = 5, threshold=None, *, data):
     knn = KNeighborsRegressor(n_neighbors=n_nearest)
     # Fit the model on the training data.
     knn.fit(train_data[training_attributes], train_data[testing_attribute])
-    
+
     # Make point predictions and Get the actual values from the test set.
     pred = knn.predict(test_data[training_attributes])
     true = array(test_data[testing_attribute])
-    
+
     # Converts
-    if threshold !=None:
+    if threshold != None:
         for i in range(len(pred)):
             if pred[i] >= threshold:
                 pred[i] = 1
             else:
                 pred[i] = 0
         pred = pred.astype(int)
-    
-    #Returns predicted(pred) and actual(true) values
+
+    # Returns predicted(pred) and actual(true) values
     return pred, true
-    
