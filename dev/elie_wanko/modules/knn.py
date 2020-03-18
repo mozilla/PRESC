a20d4ae4480146e3630cc6a0486d1f49ec54d8db
Melius <elie.wanko@gmail.com>
2020-03-18 21:41:36 +0300

    refactored for performance
    just the required functions imported

--- dev/elie_wanko/modules/knn.py
+++ dev/elie_wanko/modules/knn.py
@@ -1,10 +1,10 @@
 #Imports
-import numpy as np
-import math
+from numpy import random, array
+from math import floor
 
 from sklearn.neighbors import KNeighborsRegressor
 
-def classfier(data, cutoff = 10, n_nearest = 5):
+def classfier(data, cut_off = 10, n_nearest = 5):
     """ Returns the predicted valuescomputed from a simple majority vote of the nearest neighbors of each point.
     
     Keyword arguments:
@@ -12,27 +12,29 @@ def classfier(data, cutoff = 10, n_nearest = 5):
     cutoff(int) -- slices the data set into training and testing subsets (default = 10% for testing subset)
     n_nearest(int) -- nearest neighbors searches (default = 5)
     """
+    
+    # Split training and testing attributes
     training_attributes = data.columns[:-1]
     testing_attribute = data.columns[-1]
 
     # Randomly shuffle the index of nba.
-    random_indices = np.random.permutation(data.index)
+    random_indices = random.permutation(data.index)
 
     # Set a cutoff for how many items we want in the test set (in this case 1/10 of the items)
-    test_cutoff = math.floor(len(data)/cutoff)
+    cut_off = floor(len(data)/cut_off)
 
     # Generate the test and train set.
-    test = data.loc[random_indices[1:test_cutoff]]
-    train = data.loc[random_indices[test_cutoff:]]
+    test_data = data.loc[random_indices[1:cut_off]]
+    train_data = data.loc[random_indices[cut_off:]]
 
     # Look at the n_nearest neighbors.
     knn = KNeighborsRegressor(n_neighbors=n_nearest)
     # Fit the model on the training data.
-    knn.fit(train[training_attributes], train[testing_attribute])
+    knn.fit(train_data[training_attributes], train_data[testing_attribute])
     
     # Make point predictions and Get the actual values from the test set.
-    pred = knn.predict(test[training_attributes])
-    true = np.array(test[testing_attribute])
+    pred = knn.predict(test_data[training_attributes])
+    true = array(test_data[testing_attribute])
     
     #Returns predicted(pred) and actual(true) values
     return pred, true
