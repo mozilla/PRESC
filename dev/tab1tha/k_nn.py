d33f9076489a2bb4f7bc459cc8b6d0b843c82c44
tab1tha <tambetachere@gmail.com>
2020-03-19 13:21:04 +0100

    Black formatting applied

--- dev/tab1tha/k_nn.py
+++ dev/tab1tha/k_nn.py
@@ -2,31 +2,31 @@ from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def k_nearest(data_train, target_train, data_test, target_test):
-    #training model using the k nearest neighbor algorithm
-    #note that 'neighbor' must be spelt without including a 'u' in order for it to work
-    '''This function trains the model then uses it to predict target values of the test data. In it, the accuracy
-    of the K-nearest neighbor algorithm on this data set is computed and returned '''
-    
-    # Instantiate a k-NN classifier: knn
-    knn = KNeighborsClassifier(n_neighbors = 5)
-    y = target_train 
+    """This function trains a k nearest neighbor model then uses it to predict target values of the test data. In it, the accuracy
+    of the K-nearest neighbor algorithm on this data set is computed and returned. 
+    Note that 'neighbor' must be spelt without including a 'u' in order for it to work. """
+
+    knn = KNeighborsClassifier(n_neighbors=5)
+    y = target_train
     X = data_train
 
     # Fit the classifier to the training data
-    knn.fit(X,y)
+    knn.fit(X, y)
 
-    #predict labels of the test data
+    # predict labels of the test data
     target_pred = knn.predict(data_test.values)
-    #compute accuracy and return it
+    # compute accuracy and return it
     acc = knn.score(data_test, target_test)
     return (acc, target_pred)
 
+
 def visual_compare(data_train, target_train, data_test, target_test):
-    '''This function outputs a graph of the predictions gotten from the K-nn algorithm. In that same graph, the 
+    """This function outputs a graph of the predictions gotten from the K-nn algorithm. In that same graph, the 
     actual values are plotted. The variation between the predicted values and actual values is clearly visualised such
     that the user can choose the optimal value of the n_neighbors parameter of KNeighborsClassifier that will yied maximum
-    prediction accuracy'''
+    prediction accuracy"""
     # Setup arrays to store train and test accuracies
     neighbors = np.arange(1, 9)
     train_accuracy = np.empty(len(neighbors))
@@ -39,18 +39,18 @@ def visual_compare(data_train, target_train, data_test, target_test):
 
         # Fit the classifier to the training data
         knn.fit(data_train, target_train)
-        
-        #Compute accuracy on the training set
+
+        # Compute accuracy on the training set
         train_accuracy[i] = knn.score(data_train, target_train)
 
-        #Compute accuracy on the testing set
+        # Compute accuracy on the testing set
         test_accuracy[i] = knn.score(data_test, target_test)
 
     # Generate plot
-    plt.title('k-NN: Varying Number of Neighbors')
-    plt.plot(neighbors, test_accuracy, label = 'Testing Accuracy')
-    plt.plot(neighbors, train_accuracy, label = 'Training Accuracy')
+    plt.title("k-NN: Varying Number of Neighbors")
+    plt.plot(neighbors, test_accuracy, label="Testing Accuracy")
+    plt.plot(neighbors, train_accuracy, label="Training Accuracy")
     plt.legend()
-    plt.xlabel('Number of Neighbors')
-    plt.ylabel('Accuracy')
+    plt.xlabel("Number of Neighbors")
+    plt.ylabel("Accuracy")
     plt.show()
--- dev/tab1tha/main.py
+++ dev/tab1tha/main.py
@@ -4,28 +4,36 @@ import seaborn as sns
 import k_nn
 from sklearn.model_selection import train_test_split
 
-#import data into a pandas dataframe
-df = pd.read_csv('..\..\datasets\generated.csv')
+# import data into a pandas dataframe
+df = pd.read_csv("..\..\datasets\generated.csv")
 
-#EDA exploratory analysis of the data
-#Exploration
-print('*'*50+'HEAD'+'*'*50)
+# EDA exploratory analysis of the data
+# Exploration
+print("*" * 50 + "HEAD" + "*" * 50)
 print(df.head())
-print('*'*50+'DESCRIBE'+'*'*50)
+print("*" * 50 + "DESCRIBE" + "*" * 50)
 print(df.describe())
-print('*'*50+'INFO'+'*'*50)
+print("*" * 50 + "INFO" + "*" * 50)
 print(df.info())
-print('*'*100)
+print("*" * 100)
 
-#todo visualisation
+# todo visualisation
 
-data = df.drop('label', axis = 1)
-target = df['label']
-#split data set into train and test sets 
-data_train, data_test, target_train, target_test = train_test_split(data,target, test_size = 0.30, random_state = 10, stratify = target)
+data = df.drop("label", axis=1)
+target = df["label"]
+# split data set into train and test sets
+data_train, data_test, target_train, target_test = train_test_split(
+    data, target, test_size=0.30, random_state=10, stratify=target
+)
 
-#Evaluation of the  performance of the K-nearest neighbors prediction model
+# Evaluation of the  performance of the K-nearest neighbors prediction model
 kn_accuracy = k_nn.k_nearest(data_train, target_train, data_test, target_test)
-print('The accuracy of the k nearest neighbours algorithm on this dataset is {}'.format(kn_accuracy))
-print('The behaviour of the predicted values with respect to the actual values is as shown below:')
+print(
+    "The accuracy of the k nearest neighbours algorithm on this dataset is {}".format(
+        kn_accuracy
+    )
+)
+print(
+    "The behaviour of the predicted values with respect to the actual values is as shown below:"
+)
 k_nn.visual_compare(data_train, target_train, data_test, target_test)
