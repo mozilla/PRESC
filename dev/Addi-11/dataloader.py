6b8e71eaf6fd4f21030e01e195de7fd78f4b92da
Addi-11 <saddi.5131@gmail.com>
2020-03-23 03:23:36 +0530

    plot added

--- dev/Addi-11/dataloader.py
+++ dev/Addi-11/dataloader.py
@@ -1,6 +1,5 @@
 # importing required dependencies
 from IPython.display import HTML
-from tabulate import tabulate
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
