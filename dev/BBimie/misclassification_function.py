efc4e1382cba0d8e85c80834e3b5508a86cce7c6
Temitope Bimbo Babatola <54653980+BBimie@users.noreply.github.com>
2020-03-24 22:56:25 +0100

    added probability to misclasification visualization

--- dev/BBimie/misclassification_function.py
+++ dev/BBimie/misclassification_function.py
@@ -4,9 +4,23 @@
 # In[1]:
 
 
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+
+# In[ ]:
+
+
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.ensemble import RandomForestClassifier
+
+
+# In[ ]:
+
+
+clf = RandomForestClassifier()
 
 
 # This function takes in the test set, the target and the prediction and returns the total length of the test set and how many datapoints were missclassified
@@ -23,5 +37,46 @@ def misclassified_data(df, target, prediction):
 # In[ ]:
 
 
-
+def plot_misclassified(df, target):
+    #stratified shuffle split because the data is not balanced
+    split = StratifiedShuffleSplit(n_splits = 1, test_size=0.2, random_state=42)
+    for train_index, test_index in split.split(df, df[target]):
+        train = df.loc[train_index]
+        test = df.loc[test_index]
+        
+        #train feature
+        x = train.drop([target], axis=1)
+        #train target
+        y = train[target]
+    
+        #test feature
+        a = test.drop([target], axis=1)
+        #test target
+        b = test[target]
+    
+        #fit the random forest model
+        clf.fit(x,y)
+    
+        #make prediction and add to the test dataframe
+        predictions = clf.predict(a)
+        test['predict'] = predictions
+        
+        #get the prediction probability and make it a dataframe
+        probability = clf.predict_proba(a)
+        probability_df = pd.DataFrame(probability)
+        
+        #get the maximum probability for each prediction
+        max_prob = probability_df.max(axis=1)
+        
+        #change the indexing of the test set to start from zero
+        test.index = np.arange(0, len(test))
+        
+        #concatenate test set and max_prob
+        full = pd.concat([test, max_prob], axis=1)
+        full = full.rename(columns={0:'probability'})
+        
+        misclassified = full[full['predict'] != full['quality']]
+        
+        #visualize the maximum probability of prediction 
+        sns.catplot(x='predict', y="probability", hue="quality", data=misclassified, height=6, kind="bar")
 
