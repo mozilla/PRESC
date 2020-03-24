0a14f766bda80d52b53c18b0e3982dfdd6e5480e
Bolaji61 <balogunbolaji61@gmail.com>
2020-03-24 17:07:52 +0100

    Updating files to adhere to black formatting

--- dev/Omobolaji/Traversal-space-train-test-split/traversal_space_train_test_split.py
+++ dev/Omobolaji/Traversal-space-train-test-split/traversal_space_train_test_split.py
@@ -3,12 +3,10 @@ import sklearn
 import seaborn as sns
 
 from sklearn import metrics
-from sklearn.metrics import accuracy_score, \
-classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split, cross_val_score
 
 from sklearn.linear_model import LogisticRegression
-
 
 def traversal(estimator, data):
     """ This function takes estimator and a dataset as params and runs 
@@ -22,40 +20,48 @@ def traversal(estimator, data):
         results: A table of results with different test 
                 sizes and their accuracy score. 
     """
-    
+
     results = list()
-    
-    features = data.drop(['Class'], axis=1)
+
+    features = data.drop(["Class"], axis=1)
     target = data.Class
-    
-    for split in range(10, 100, 10):  #range(start, stop, step)
-        split = split/100  #convert to decimal format
-        train_features, test_features, train_target, test_target = \
-        train_test_split(features, target, test_size=split, random_state=42)
-        
-        #fit the training data into the model
+
+    for split in range(10, 100, 10):  # range(start, stop, step)
+        split = split / 100  # convert to decimal format
+        train_features, test_features, train_target, test_target = train_test_split(
+            features, target, test_size=split, random_state=42
+        )
+
+        # fit the training data into the model
         estimator.fit(train_features, train_target)
-    
-        #store the predicted values of test data
+
+        # store the predicted values of test data
         predict_target = estimator.predict(test_features)
-        
-        #Evaluate accuracy score
+
+        # Evaluate accuracy score
         accuracy = accuracy_score(test_target, predict_target)
-        
-        #convert the score to a percentage and round to two decimal places
-        accuracy = round(accuracy*100, 2) 
-        
-        #Compute confusion matrix
+
+        # convert the score to a percentage and round to two decimal places
+        accuracy = round(accuracy * 100, 2)
+
+        # Compute confusion matrix
         cm = confusion_matrix(test_target, predict_target)
-    
+
         # Calculate the classification report
         cr = classification_report(test_target, predict_target)
-        
-        #append to an empty list created
+
+        # append to an empty list created
         results.append([split, accuracy, cm, cr])
-        
-    
-    #convert the results to tabular form using Pandas Dataframe function
-    results = pd.DataFrame(results, columns = ['Test-Size', 'Accuracy Score(%)', 'Confusion Matrix', 'Classification Report']) 
-#     results.explode('Classification Report')   this function doesnt seem to work
+
+    # convert the results to tabular form using Pandas Dataframe function
+    results = pd.DataFrame(
+        results,
+        columns=[
+            "Test-Size",
+            "Accuracy Score(%)",
+            "Confusion Matrix",
+            "Classification Report",
+        ],
+    )
+    #     results.explode('Classification Report')   this function doesnt seem to work
     return results
