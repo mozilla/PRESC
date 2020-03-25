18e3341caa512b8869a1da44a808630ae43aeabc
Bolaji61 <balogunbolaji61@gmail.com>
2020-03-25 03:40:23 +0100

    Fixed issue #3 completely

--- dev/Omobolaji/Traversal-space-train-test-split/traversal_space_train_test_split.py
+++ dev/Omobolaji/Traversal-space-train-test-split/traversal_space_train_test_split.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import sklearn
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 from sklearn import metrics
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import (
+    accuracy_score,
+    classification_report,
+    confusion_matrix,
+    f1_score,
+    precision_score,
+    recall_score,
+)
 from sklearn.model_selection import train_test_split, cross_val_score
 
 from sklearn.linear_model import LogisticRegression
@@ -20,7 +28,7 @@ def traversal(estimator, data):
         results: A table of results with different test 
                 sizes and their accuracy score. 
     """
-
+    #     metrics = " \n "
     results = list()
 
     features = data.drop(["Class"], axis=1)
@@ -28,6 +36,7 @@ def traversal(estimator, data):
 
     for split in range(10, 100, 10):  # range(start, stop, step)
         split = split / 100  # convert to decimal format
+        train_size = 1 - split
         train_features, test_features, train_target, test_target = train_test_split(
             features, target, test_size=split, random_state=42
         )
@@ -44,24 +53,34 @@ def traversal(estimator, data):
         # convert the score to a percentage and round to two decimal places
         accuracy = round(accuracy * 100, 2)
 
-        # Compute confusion matrix
-        cm = confusion_matrix(test_target, predict_target)
+        # Evaluate the weighted(average) precision, recall and F1 score
+        """
+        In calculating these scores, the parameter,
+        "average" can be equated to micro(the smallest value), macro(the largest value) 
+        or weighted(which is an average of the values). In order to have a good overall
+        view of our scores for each test sizes, "weighted" is chosen.
+        """
 
-        # Calculate the classification report
-        cr = classification_report(test_target, predict_target)
+        precision = precision_score(test_target, predict_target, average="weighted")
+        precision = round(precision * 100, 2)
+        recall = recall_score(test_target, predict_target, average="weighted")
+        recall = round(recall * 100, 2)
+        f1 = f1_score(test_target, predict_target, average="weighted")
+        f1 = round(f1 * 100, 2)
 
         # append to an empty list created
-        results.append([split, accuracy, cm, cr])
+        results.append([train_size, split, accuracy, precision, recall, f1])
 
     # convert the results to tabular form using Pandas Dataframe function
     results = pd.DataFrame(
         results,
         columns=[
-            "Test-Size",
+            "Train Size",
+            "Test Size",
             "Accuracy Score(%)",
-            "Confusion Matrix",
-            "Classification Report",
+            "Precision(%)",
+            "Recall(%)",
+            "F1_Score(%)",
         ],
     )
-    #     results.explode('Classification Report')   this function doesnt seem to work
     return results
