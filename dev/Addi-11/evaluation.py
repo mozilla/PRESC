fca21a2b2aa1f6655a4dda4bceef794d032d62c8
Addi-11 <47007177+Addi-11@users.noreply.github.com>
2020-03-26 07:27:59 +0530

    Learning from Misclassifications. (#112)
    * visual for eeg
    
    * code restructured
    
    * #3 data-split space mapped
    
    * fixes issue3
    
    * studied data splits for all classifiers
    
    * added graph in the loop
    
    * docstrings added
    
    * validation sets added
    
    * formatting
    
    * evaluated all classifiers
    
    * compared models
    
    * result added
    
    * indexed
    
    * removed plot-recall-curve
    
    * learning-curve
    
    * added models
    
    * env refresh
    
    * final estimate added
    
    * black formats
    
    * conclusion added

--- dev/Addi-11/evaluation.py
+++ dev/Addi-11/evaluation.py
@@ -1,14 +1,19 @@
 # This file contain evaluation metrics for the model
 
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score,confusion_matrix, plot_confusion_matrix,average_precision_score,precision_recall_curve, plot_precision_recall_curve, f1_score, precision_score, recall_score
+from sklearn.metrics import (
+    accuracy_score,
+    average_precision_score,
+    f1_score,
+    precision_score,
+    recall_score,
+)
 import pandas as pd
 
 from sklearn.utils import class_weight
 
-
 def evaluate(classifier, x_val, y_val):
-    '''
+    """
     This function predicts the values for a trained model and calculates the evaluation metrics accuracy, precision, recall, f_score on the validation set. 
     Parameters :
         classifier : trained classification model
@@ -21,7 +26,7 @@ def evaluate(classifier, x_val, y_val):
         recall : float
         f_score : float
         y_score : array-like of length n_features, 
-    '''
+    """
     y_score = classifier.predict(x_val)
     accuracy = accuracy_score(y_val, y_score)
     f_score = f1_score(y_val, y_score)
@@ -29,4 +34,3 @@ def evaluate(classifier, x_val, y_val):
     recall = recall_score(y_val, y_score)
 
     return accuracy, precision, recall, f_score, y_score
-
