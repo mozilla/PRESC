b1fec72a83a2be6cfbc7d83ed7fa9ff0b8d1ec21
tab1tha <tambetachere@gmail.com>
2020-04-03 20:07:18 +0100

    Visualization of class probabilities

--- dev/tab1tha/compare_estimators.py
+++ dev/tab1tha/compare_estimators.py
@@ -4,8 +4,10 @@ an optimal model for the dataset of interest. """
 
 import k_nn
 import s_v_m
+import explore_datav2
 import pandas as pd
 import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
 from load_dataset import load_dataset
 from sklearn.metrics import confusion_matrix
@@ -18,33 +20,46 @@ df = load_dataset(filename)
 data = df.drop("Class", axis=1)
 target = df["Class"]
 
-data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.30, stratify=target, random_state=42)
+data_train, data_test, target_train, target_test = train_test_split(
+    data, target, test_size=0.30, stratify=target, random_state=42
+)
 """scaling improves model performance significantly however it raised a questionable TypeError
 data_train = StandardScaler(data_train)
 data_test = StandardScaler(data_test)"""
-acc_knn, target_pred_knn = k_nn.k_nearest(data_train=data_train, target_train=target_train, data_test=data_test, target_test=target_test)
-acc_svc, target_pred_svc = s_v_m.s_vee_c(data_train, data_test,target_train, target_test)
+acc_knn, target_pred_knn = k_nn.k_nearest(
+    data_train=data_train,
+    target_train=target_train,
+    data_test=data_test,
+    target_test=target_test,
+)
+acc_svc, target_pred_svc = s_v_m.s_vee_c(
+    data_train, data_test, target_train, target_test
+)
 """The K-Nearest neighbor and support vector classifier models are trained and tested with tuned hyperparameters.
 Their accuracy scores and prediction values are extracted from their respective functions. """
+
 
 for a in [target_pred_knn, target_pred_svc]:
     a = list(a)
 
-#target_pred_knn = list(target_pred_knn)
-#target_pred_svc = list(target_pred_svc)
-conf_matrix_knn = confusion_matrix(target_test, target_pred_knn)   
-conf_matrix_svc = confusion_matrix(target_test, target_pred_svc)   
+# target_pred_knn = list(target_pred_knn)
+# target_pred_svc = list(target_pred_svc)
+conf_matrix_knn = confusion_matrix(target_test, target_pred_knn)
+conf_matrix_svc = confusion_matrix(target_test, target_pred_svc)
 
-combi_dict = {"Ref":list(target_test)}
+plt.subplot(2, 1, 1)
+explore_datav2.confu_matrix_display(conf_matrix_knn, target, "KNN")
+plt.subplot(2, 1, 2)
+explore_datav2.confu_matrix_display(conf_matrix_svc, target, "SVC")
+plt.show()
+
+# Create a dataframe that contains true values and the corresponding predicted values per model
+combi_dict = {"Ref": list(target_test)}
 compare_df = pd.DataFrame(combi_dict, columns=["Ref"])
 compare_df["KNN"] = target_pred_knn
 compare_df["SVC"] = target_pred_svc
 
-#print(compare_df)
+# print(compare_df)
 print(compare_df.Ref.value_counts())
 print(compare_df.KNN.value_counts())
 print(compare_df.SVC.value_counts())
-
-#confu_matrix_display(conf_matrix_knn, target_vals)
-#sns.pairplot(df)
-#plt.show()
--- dev/tab1tha/explore_datav2.py
+++ dev/tab1tha/explore_datav2.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import numpy as np
 
 
 def raw(df):
@@ -19,6 +20,7 @@ def raw(df):
     print(df.info())
     print("*" * 100)
 
+
 def raw_more(df):
     """This function goes a step further from the raw() function in easing the understanding of the 
     dataset and its data. It puts more emphasis on the interrelation between variables and gives more
@@ -27,6 +29,7 @@ def raw_more(df):
     print(df.groupby("Class").mean())
     sns.heatmap(df.corr())
     plt.show()
+
 
 def graph_visualize(df, targetname):
     """This function plots a pair plot that provides a detailed view of interrelationships in
@@ -49,13 +52,15 @@ def violin_visualize(df):
     bp = ax.violinplot(df["label"])
     plt.show(bp)
 
-def confu_matrix_display(conf_matrix, target_vals):
+
+def confu_matrix_display(conf_matrix, target, title):
     """This functions receives a confusion matrix and the list of unique target variable values as 
     parameters. The target values serve as a source of labels to ease understanding of the confusion 
     matrix that is plotted out as output. """
-    df_conf_matrix = pd.DataFrame(conf_matrix, index = target_vals, columns=target_vals)
+    target_vals = np.unique(target)
+    df_conf_matrix = pd.DataFrame(conf_matrix, index=target_vals, columns=target_vals)
     sns.heatmap(df_conf_matrix, annot=True, fmt="d")
     plt.xlabel("Predicted values")
     plt.ylabel("Actual values")
-    plt.title("Confusion matrix")
+    plt.title(title + " confusion matrix")
     plt.show()
