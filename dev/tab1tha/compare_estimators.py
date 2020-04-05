9bb53a25286575b173aff6bf52e6d62789330557
tab1tha <tambetachere@gmail.com>
2020-04-05 13:21:55 +0100

    Visualisation of class probabilities per classifier and svm optimisation module modified

--- dev/tab1tha/compare_estimators.py
+++ dev/tab1tha/compare_estimators.py
@@ -2,64 +2,56 @@
 when they are trained and used on the same dataset. It is the first step in guiding the choice of 
 an optimal model for the dataset of interest. """
 
-import k_nn
-import s_v_m
-import explore_datav2
-import pandas as pd
-import seaborn as sns
-import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-from load_dataset import load_dataset
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+import numpy as np
 
-filename = "vehicles.csv"
-df = load_dataset(filename)
+def class_probs(labels, estimators, data_test):
 
-data = df.drop("Class", axis=1)
-target = df["Class"]
+    preds = []
+    prob0s = []
+    prob1s = []
+    for clf in estimators:
+        pred = clf.predict_proba(data_test)
+        preds.append(pred)
+        
+        prob0 = np.mean(pred[:,0])
+        prob0s.append(prob0)
+        
+        prob1 = np.mean(pred[:,1])
+        prob1s.append(prob1)
+        
 
-data_train, data_test, target_train, target_test = train_test_split(
-    data, target, test_size=0.30, stratify=target, random_state=42
-)
-"""scaling improves model performance significantly however it raised a questionable TypeError
-data_train = StandardScaler(data_train)
-data_test = StandardScaler(data_test)"""
-acc_knn, target_pred_knn = k_nn.k_nearest(
-    data_train=data_train,
-    target_train=target_train,
-    data_test=data_test,
-    target_test=target_test,
-)
-acc_svc, target_pred_svc = s_v_m.s_vee_c(
-    data_train, data_test, target_train, target_test
-)
-"""The K-Nearest neighbor and support vector classifier models are trained and tested with tuned hyperparameters.
-Their accuracy scores and prediction values are extracted from their respective functions. """
+    x = np.arange(len(labels))  # the label locations
+    width = 0.35  # the width of the bars
+
+    fig, ax = plt.subplots(figsize=(6,7))
+    rects1 = ax.bar(x - width/2, prob0s, width, label='Class0')
+    rects2 = ax.bar(x + width/2, prob1s, width, label='Class1')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Mean probability')
+    ax.set_title('Probability by estimator and class')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
 
 
-for a in [target_pred_knn, target_pred_svc]:
-    a = list(a)
+    def autolabel(rects):
+        """Attach a text label above each bar in *rects*, displaying its height."""
+        for rect in rects:
+            height = rect.get_height()
+            height = round(height, 2)
+            ax.annotate('{}'.format(height),
+                        xy=(rect.get_x() + rect.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom')
 
-# target_pred_knn = list(target_pred_knn)
-# target_pred_svc = list(target_pred_svc)
-conf_matrix_knn = confusion_matrix(target_test, target_pred_knn)
-conf_matrix_svc = confusion_matrix(target_test, target_pred_svc)
 
-plt.subplot(2, 1, 1)
-explore_datav2.confu_matrix_display(conf_matrix_knn, target, "KNN")
-plt.subplot(2, 1, 2)
-explore_datav2.confu_matrix_display(conf_matrix_svc, target, "SVC")
-plt.show()
+    autolabel(rects1)
+    autolabel(rects2)
 
-# Create a dataframe that contains true values and the corresponding predicted values per model
-combi_dict = {"Ref": list(target_test)}
-compare_df = pd.DataFrame(combi_dict, columns=["Ref"])
-compare_df["KNN"] = target_pred_knn
-compare_df["SVC"] = target_pred_svc
+    fig.tight_layout()
 
-# print(compare_df)
-print(compare_df.Ref.value_counts())
-print(compare_df.KNN.value_counts())
-print(compare_df.SVC.value_counts())
+    plt.show()
--- dev/tab1tha/s_v_m.py
+++ dev/tab1tha/s_v_m.py
@@ -6,7 +6,9 @@ from sklearn.metrics import accuracy_score
 def tune(data_train, target_train):
     """This function performs cross validation in order to find the hyperparameter value that will 
     lead to maximum model performance. """
-    param_grid = {'C':[1,10,100,1000], 'gamma':[0.001,0.0001], 'kernel':["rbf"]}
+    param_grid = [{'C': [1, 10, 100, 1000], 'kernel': ['linear']}, {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},]
+    """The parameter grid takes into consideration both the linear and rbf kernels. 
+    This gives statistical backing to the final choice of a kernel to use. """
     svc = SVC()
     #The verbose parameter can be added to GridSearchCV so that its progress can be visible.
     optimized_svc = GridSearchCV(estimator=svc, param_grid=param_grid, cv=5, iid=True)
