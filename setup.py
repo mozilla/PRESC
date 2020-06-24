6dbad8a68442c0a709894f9d5a19b89ac61715cf
Victor Ng <victor@crankycoder.com>
2020-06-24 13:59:56 -0400

    Move old code to archive

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     setup_requires=["setuptools_scm", "pytest-runner"],
     tests_require=["pytest"],
     include_package_data=True,
-    packages=find_packages(exclude=["tests", "tests/*"]),
+    packages=find_packages(exclude=["archive", "tests", "tests/*"]),
     description="Performance Robustness Evaluation for Statistical Classifiers",
     author="Mozilla Corporation",
     author_email="mroviraesteva@mozilla.com",
