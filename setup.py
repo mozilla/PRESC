56763a6f21a90b853950fbb70c22c36011105642
Victor Ng <victor@crankycoder.com>
2020-07-08 13:15:54 -0400

    Add PyPI upload support
    * twine has been added as a dependency to support PyPI uploads
    * `upload` target added to Makefile
    * PyPI upload docs added to README
    * setup.py modified to include a long_description for the PyPI index

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import find_packages, setup
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="PRESC",
     use_scm_version=False,
-    version="0.1.0",
+    version="0.1.1",
     setup_requires=["setuptools_scm", "pytest-runner"],
     tests_require=["pytest"],
     include_package_data=True,
     packages=find_packages(exclude=["archive", "tests", "tests/*"]),
     description="Performance Robustness Evaluation for Statistical Classifiers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
     author="Mozilla Corporation",
     author_email="mroviraesteva@mozilla.com",
     url="https://github.com/mozilla/presc",
