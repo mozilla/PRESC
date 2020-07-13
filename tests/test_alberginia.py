b92fda02f019ecbdc3e783bf54a459172a686eed
Dave Zeber <dzeber@mozilla.com>
2020-07-13 18:54:55 -0500

    Disable testing for now after moving code to archive

--- tests/test_alberginia.py
+++ tests/test_alberginia.py
@@ -1,6 +1,7 @@
-from presc.alberginia.data_exploration import load_file
+# from archive.alberginia.data_exploration import load_file
 
 
 def test_load_file():
-    fn = "datasets/generated.csv"
-    load_file(fn)
+#     fn = "datasets/generated.csv"
+#     load_file(fn)
+    pass
