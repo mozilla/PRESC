52c7517d1b9bcd2cf4c7b73af628be272f39b50c
AmandaYang <yuanyuan.yang@alumni.ubc.ca>
2020-10-09 21:41:46 +0800

    removed the transformer attribute and functions, imlemented get_train_dataset() and get_raw_dataset(); changed reading the dataset file by name to by file path

--- datasets/dataset_wrapper.py
+++ datasets/dataset_wrapper.py
@@ -1,78 +1,54 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 """
 Dataset Wrapper API 
 
-Provides some functionalities to access and preprocess the datasets.
-functionalities: 
-1. reading and writing from files - DONE
-2. access train/test of the data  - DONE 
-3. accessing feature columns
-4. label column accessing pre-split train/test sets
-5. TBD
+Provides some functionalities to access the datasets as a pandas DataFrame object.
+You can access the raw dataset as well as the the feature and label columns.
+You can also split the dataset into train and test datasets, and access them.
 
 """
-# Helper functions:
-
 class DatasetWrapper(object):
-    name = ""
-    dataset = None
-
     X,y = None, None
     X_train, X_test, y_train, y_test = None, None, None, None
-    
-    transformer = None
-    X_train_transformed ,X_test_transformed = None, None
 
-    def __init__(self,test_dataset_name:str) -> None:
+    def __init__(self,dataset_file:str) -> None:
         """
         Description:
-            To initialize a dataset object. 
-            The client may only choose a dataset from the already existed datasets.
+            To initialize a dataset object using the link found in the datasets folder 
 
         Args:
             test_dataset_name (string): a string that is the name of one of the test/datasets
-        """  
-        # Datasets from the datasets/ folder     
-        self._datasets = ("generated", "vehicles", "eeg", "winequality", "defaults")
-
-        if test_dataset_name not in self._datasets:
-            raise ValueError("The test dataset name you chose was not in the datasets/ folder.")
-        else:
-            # TODO: do I always drop quality column? Or is it dataset specific
-            self.name = test_dataset_name
-        
-        # read the dataset from url
-        self.dataset = pd.read_csv("https://github.com/mozilla/PRESC/blob/master/datasets/" + self.name + ".csv")
+        """
+        # read the dataset from file path
+        try:
+            self._dataset = pd.read_csv(dataset_file)
+        except IOError as e:
+            print(e + ''' Please check the file path that your passed is correct. \n 
+            It should be in the format dataset/xxx.csv if you are currently in the PRESC folder.''')
 
         # set X,y
-        self.X,self.y = self.dataset.iloc[:,:-1], self.dataset.iloc[:,-1]
+        self.X,self.y = self._dataset.iloc[:,:-1], self._dataset.iloc[:,-1]
 
-    def split_test_train(self,test_size = 0.2, random_state = 0):
-        
+    def split_test_train(self,test_size:float = 0.2, random_state:int = 0) -> None:
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, 
         test_size=test_size, random_state=random_state)
 
-    def get_label(self):
+    def get_raw_dataset(self) -> object:
+        return self._dataset
+
+    def get_label(self) -> object:
         return self.y
 
-    def get_features(self):
+    def get_features(self) -> object:
         return self.X
 
-    # TODO: should implement dataset-specific processor
-    def get_feature_processer(self):
-        self.transformer = StandardScaler().fit(self.X_train)
-        return self.transformer
-
-    def transform_dataset(self):
-        self.X_train_transformed  = self.transformer.transform(self.X_train)
-        self.X_test_transformed  = self.transformer.transform(self.X_test)
-
-    def get_test_dataset(self):
+    def get_test_dataset(self) -> object:
         dataset_test = self.X_test.merge(self.y_test, 
         left_index=True, right_index=True, how='left') 
         return dataset_test
 
-    
-    
+    def get_train_dataset(self) -> object:
+        dataset_train = self.X_train.merge(self.y_train, 
+        left_index=True, right_index=True, how='left') 
+        return dataset_train
