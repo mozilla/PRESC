209a60293e48c2b1ea815416c5b421d5ef8dda97
AmandaYang <yuanyuan.yang@alumni.ubc.ca>
2020-10-10 19:23:41 +0800

    added subset parameter for feature and label getters for if the user wishes to get them only for test/train datasets. wrote tests for dataset wrapper class

--- datasets/dataset_wrapper.py
+++ datasets/dataset_wrapper.py
@@ -9,6 +9,7 @@ You can also split the dataset into train and test datasets, and access them.
 
 """
 class DatasetWrapper(object):
+    _dataset= None
     X,y = None, None
     X_train, X_test, y_train, y_test = None, None, None, None
 
@@ -20,15 +21,18 @@ class DatasetWrapper(object):
         Args:
             test_dataset_name (string): a string that is the name of one of the test/datasets
         """
-        # read the dataset from file path
+        
         try:
+            # read the dataset from file path
             self._dataset = pd.read_csv(dataset_file)
-        except IOError as e:
-            print(e + ''' Please check the file path that your passed is correct. \n 
-            It should be in the format dataset/xxx.csv if you are currently in the PRESC folder.''')
 
-        # set X,y
-        self.X,self.y = self._dataset.iloc[:,:-1], self._dataset.iloc[:,-1]
+            # set X,y
+            self.X,self.y = self._dataset.iloc[:,:-1], self._dataset.iloc[:,-1]
+        except (FileNotFoundError, IOError):
+            print(
+            'Please check the file path that your passed is valid.\n'
+            + 'It should be in the format dataset/xxx.csv if you are currently in the PRESC folder.\n')
+            raise
 
     def split_test_train(self,test_size:float = 0.2, random_state:int = 0) -> None:
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, 
@@ -37,10 +41,24 @@ class DatasetWrapper(object):
     def get_raw_dataset(self) -> object:
         return self._dataset
 
-    def get_label(self) -> object:
+    def get_label(self, subset:str = None) -> object:
+        if subset == "test":
+            return self.y_test
+        elif subset == "train":
+            return self.y_train
+        elif subset:
+            raise ValueError("Subset dataset can only be test or train.")
+        
         return self.y
 
-    def get_features(self) -> object:
+    def get_features(self, subset:str = None) -> object:
+        if subset == "test":
+            return self.X_test
+        elif subset == "train":
+            return self.X_train
+        elif subset:
+            raise ValueError("Subset dataset can only be test or train.")
+        
         return self.X
 
     def get_test_dataset(self) -> object:
@@ -52,3 +70,4 @@ class DatasetWrapper(object):
         dataset_train = self.X_train.merge(self.y_train, 
         left_index=True, right_index=True, how='left') 
         return dataset_train
+
--- tests/test_dataset_wrapper.py
+++ tests/test_dataset_wrapper.py
@@ -1,4 +1,69 @@
 import numpy as np
 import pandas as pd
 import pytest
+from sklearn.model_selection import train_test_split
 
+from datasets.dataset_wrapper import DatasetWrapper
+
+wine_data_path = "../datasets/winequality.csv"
+random_state, test_size = 0, 0.2
+
+# test invalid initailization of DatasetWrapper object
+def test_fie_does_not_exist():
+    with pytest.raises(FileNotFoundError):
+        DatasetWrapper("datasets/winequalit.csv")
+
+def test_fie_format_incorrect():
+    with pytest.raises(IOError):
+        DatasetWrapper("datasets/README.md")
+
+# test winequality dataset and functionalities: 
+@pytest.fixture
+def wine_dataset_wrapper():
+    dataset_wrapper = DatasetWrapper(wine_data_path)
+    return dataset_wrapper
+
+@pytest.fixture
+def expected_wine_dataset():
+    expected_dataset = pd.read_csv(wine_data_path)
+    return expected_dataset
+
+def test_get_raw_dataset(wine_dataset_wrapper,expected_wine_dataset):
+    actual_dataset = wine_dataset_wrapper.get_raw_dataset()
+    assert actual_dataset.equals(expected_wine_dataset)
+
+def test_get_features(wine_dataset_wrapper,expected_wine_dataset):
+    actual_X = wine_dataset_wrapper.get_features()
+    assert actual_X.equals(expected_wine_dataset.iloc[:,:-1])
+
+def test_get_label(wine_dataset_wrapper,expected_wine_dataset):
+    actual_y = wine_dataset_wrapper.get_label()
+    assert actual_y.equals(expected_wine_dataset["recommend"])
+
+def test_split_test_train(wine_dataset_wrapper,expected_wine_dataset):
+    wine_dataset_wrapper.split_test_train()
+    
+    expected_train_X, expected_test_X, expected_train_y, expected_test_y =  train_test_split(   expected_wine_dataset.iloc[:,:-1],
+                                                                                                expected_wine_dataset["recommend"],
+                                                                                                random_state = random_state,
+                                                                                                test_size = test_size )
+
+    expected_train = expected_train_X.merge(expected_train_y,left_index=True, right_index=True, how='left')
+    expected_test = expected_test_X.merge(expected_test_y,left_index=True, right_index=True, how='left')
+
+    assert expected_train_X.equals(wine_dataset_wrapper.get_features(subset="train"))
+    assert expected_test_X.equals(wine_dataset_wrapper.get_features(subset="test"))
+    assert expected_train_y.equals(wine_dataset_wrapper.get_label(subset="train"))
+    assert expected_test_y.equals(wine_dataset_wrapper.get_label(subset="test"))
+
+    assert expected_train.equals(wine_dataset_wrapper.get_train_dataset())
+    assert expected_test.equals(wine_dataset_wrapper.get_test_dataset())
+    
+# test invalid subset for get_features and get_label
+def test_invalid_get_features(wine_dataset_wrapper):
+    with pytest.raises(ValueError):
+        wine_dataset_wrapper.get_features(subset = "hello")
+
+def test_invalid_get_label(wine_dataset_wrapper):
+    with pytest.raises(ValueError):
+        wine_dataset_wrapper.get_label(subset = "wine")
