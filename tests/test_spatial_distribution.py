b5d9df10d85355143e3c619364ec553e72dde826
jdiego-miyashiro <>
2020-11-28 13:00:59 -0700

    fixed a bug in goodall2, added several tests

--- tests/test_spatial_distribution.py
+++ tests/test_spatial_distribution.py
@@ -7,7 +7,7 @@ import pytest
 
 
 @pytest.fixture
-def catd_analysis():
+def random_catd_analysis():
     toy_data = []
     column_names = []
     lower_upper_alphabet = string.ascii_letters
@@ -21,7 +21,7 @@ def catd_analysis():
         for feature in range(0, 9):
             data_entry.append(rd.choice(lower_upper_alphabet))
 
-        data_entry.append(rd.choice([0, 1]))
+        data_entry.append(rd.choice([0, 1]))  # appending the classification row
         data_entry = tuple(data_entry)
         toy_data.append(data_entry)
 
@@ -30,62 +30,155 @@ def catd_analysis():
     y_pred = np.random.randint(2, size=len(df))
     print(y_pred)
 
-    catd_analysis = catd.SpatialDistribution(df, df["label"], y_pred)
+    catd_random = catd.SpatialDistribution(df, df["label"], y_pred)
+    return catd_random
+
+
+@pytest.fixture
+def catd_analysis():
+    column_names = []
+    for i in range(5):
+        name = "feature_" + str(i)
+        column_names.append(name)
+    column_names.append("label")
+
+    toy_data = [
+        ("p", "t", "x", "v", "q", "p"),
+        ("e", "y", "x", "y", "q", "p"),
+        ("p", "x", "s", "y", "t", "p"),
+        ("e", "x", "s", "y", "t", "a"),
+        ("e", "b", "s", "w", "t", "a"),
+        ("p", "x", "y", "w", "t", "p"),
+    ]
+
+    df = pd.DataFrame(toy_data, columns=column_names)
+    y_pred = ["0", "1", "0", "1", "0", "1"]
+    y_true = ["0", "1", "1", "1", "0", "0"]
+    catd_analysis = catd.SpatialDistribution(df, y_pred, y_true)
+
     return catd_analysis
 
 
-# Distances should be symmetric
-def test_lin(catd_analysis):
-    dpoint1 = catd_analysis.get_datapoint(
-        rd.randint(0, catd_analysis.get_data_len() - 1)
-    )
-    dpoint2 = catd_analysis.get_datapoint(
-        rd.randint(0, catd_analysis.get_data_len() - 1)
-    )
-
-    assert catd_analysis.lin(dpoint1, dpoint2) == catd_analysis.lin(dpoint2, dpoint1)
-
-
-def test_overlap(catd_analysis):
-    dpoint1 = catd_analysis.get_datapoint(
-        rd.randint(0, catd_analysis.get_data_len() - 1)
-    )
-    dpoint2 = catd_analysis.get_datapoint(
-        rd.randint(0, catd_analysis.get_data_len() - 1)
-    )
-
-    assert catd_analysis.overlap(dpoint1, dpoint2) == catd_analysis.overlap(
-        dpoint2, dpoint1
-    )
-
-
 def test_goodall2(catd_analysis):
-    dpoint1 = catd_analysis.get_datapoint(
-        rd.randint(0, catd_analysis.get_data_len() - 1)
-    )
-    dpoint2 = catd_analysis.get_datapoint(
-        rd.randint(0, catd_analysis.get_data_len() - 1)
-    )
-
-    assert catd_analysis.goodall2(dpoint1, dpoint2) == catd_analysis.goodall2(
-        dpoint2, dpoint1
-    )
+    dpoint1 = catd_analysis.get_datapoint(-1)
+    dpoint2 = catd_analysis.get_datapoint(-2)
+    hand_calculated_value = 0.3222222
+    output_value = catd_analysis.goodall2(dpoint1, dpoint2)
+    np.testing.assert_approx_equal(hand_calculated_value, output_value)
 
 
 def test_goodall3(catd_analysis):
-    dpoint1 = catd_analysis.get_datapoint(
-        rd.randint(0, catd_analysis.get_data_len() - 1)
-    )
-    dpoint2 = catd_analysis.get_datapoint(
-        rd.randint(0, catd_analysis.get_data_len() - 1)
-    )
-    assert catd_analysis.goodall3(dpoint1, dpoint2) == catd_analysis.goodall3(
-        dpoint2, dpoint1
-    )
+    dpoint1 = catd_analysis.get_datapoint(0)
+    dpoint2 = catd_analysis.get_datapoint(1)
+    hand_calculated_value = (1 / 6) * (((2 * (14 / 15))) + (1 - (6.0 / 15)))
+    output_value = catd_analysis.goodall3(dpoint1, dpoint2)
+    print(hand_calculated_value)
+    print(output_value)
+    np.testing.assert_approx_equal(hand_calculated_value, output_value)
 
 
-def test_distance_histogram(catd_analysis):
-    catd_analysis.plot_distance_histogram("overlap", histo_sample=1, distance_sample=1)
+def test_lin(catd_analysis):
+    dpoint1 = catd_analysis.get_datapoint(2)
+    dpoint2 = catd_analysis.get_datapoint(3)
+    hand_calculated_value = (
+        2 * np.log(1)
+        + 2 * np.log((3 / 6) * (3 / 6) * (3 / 6) * (4 / 6))
+        + 2 * np.log((4 / 6) + (2 / 6))
+    )
+    lin_weight = (
+        (np.log(3 / 6) + np.log(3 / 6))
+        + 2 * np.log(3 / 6)
+        + 2 * np.log(3 / 6)
+        + 2 * np.log(3 / 6)
+        + 2 * (np.log(4 / 6))
+        + (np.log(4 / 6) + np.log(2 / 6))
+    )
+    hand_calculated_value = (1 / lin_weight) * hand_calculated_value
+    output_value = catd_analysis.lin(dpoint1, dpoint2)
+    np.testing.assert_approx_equal(hand_calculated_value, output_value)
+
+
+def test_overlap(catd_analysis):
+    dpoint1 = catd_analysis.get_datapoint(4)
+    dpoint2 = catd_analysis.get_datapoint(5)
+    hand_calculated_value = 2 / 6
+    output_value = catd_analysis.overlap(dpoint1, dpoint2)
+    np.testing.assert_approx_equal(hand_calculated_value, output_value)
+
+
+""" Distances should be symmetric """
+
+
+def test_goodall2_symmetry(random_catd_analysis):
+    dpoint1 = random_catd_analysis.get_datapoint(
+        rd.randint(0, random_catd_analysis.get_data_len() - 1)
+    )
+    dpoint2 = random_catd_analysis.get_datapoint(
+        rd.randint(0, random_catd_analysis.get_data_len() - 1)
+    )
+
+    goodall2_left = random_catd_analysis.goodall2(dpoint1, dpoint2)
+    goodall2_right = random_catd_analysis.goodall2(dpoint1, dpoint2)
+    np.testing.assert_approx_equal(goodall2_left, goodall2_right)
+
+
+def test_goodall3_symmetry(random_catd_analysis):
+    dpoint1 = random_catd_analysis.get_datapoint(
+        rd.randint(0, random_catd_analysis.get_data_len() - 1)
+    )
+    dpoint2 = random_catd_analysis.get_datapoint(
+        rd.randint(0, random_catd_analysis.get_data_len() - 1)
+    )
+    goodall3_left = random_catd_analysis.goodall3(dpoint1, dpoint2)
+    goodall3_right = random_catd_analysis.goodall3(dpoint1, dpoint2)
+    np.testing.assert_approx_equal(goodall3_left, goodall3_right)
+
+
+def test_lin_symmetry(random_catd_analysis):
+    dpoint1 = random_catd_analysis.get_datapoint(
+        rd.randint(0, random_catd_analysis.get_data_len() - 1)
+    )
+    dpoint2 = random_catd_analysis.get_datapoint(
+        rd.randint(0, random_catd_analysis.get_data_len() - 1)
+    )
+    lin_left = random_catd_analysis.lin(dpoint1, dpoint2)
+    lin_right = random_catd_analysis.lin(dpoint1, dpoint2)
+    np.testing.assert_approx_equal(lin_left, lin_right)
+
+
+def test_overlap_symmetry(random_catd_analysis):
+    dpoint1 = random_catd_analysis.get_datapoint(
+        rd.randint(0, random_catd_analysis.get_data_len() - 1)
+    )
+    dpoint2 = random_catd_analysis.get_datapoint(
+        rd.randint(0, random_catd_analysis.get_data_len() - 1)
+    )
+    overlap_left = random_catd_analysis.lin(dpoint1, dpoint2)
+    overlap_right = random_catd_analysis.lin(dpoint1, dpoint2)
+    np.testing.assert_approx_equal(overlap_left, overlap_right)
+
+
+def test_array_of_distance_intersection(random_catd_analysis):
+    """The intersection of the indexes of the points returned in the tuple list
+    must have a set intersection of len = 0"""
+    dpoint1 = random_catd_analysis.get_datapoint(
+        rd.randint(0, random_catd_analysis.get_data_len() - 1)
+    )
+    misclass_only = random_catd_analysis.array_of_distance(
+        dpoint1, "lin", misclass_only=True
+    )
+    trueclass_only = random_catd_analysis.array_of_distance(
+        dpoint1, "lin", correct_only=True
+    )
+    miss_index = [tuple[1] for tuple in misclass_only]
+    diss_index = [tuple[1] for tuple in trueclass_only]
+    set_intersection = set(miss_index) & set(diss_index)
+
+    assert len(set_intersection) == 0
+
+
+def test_avg_distance_histogram(catd_analysis):
+    catd_analysis.plot_avg_distance_histogram("lin", histo_sample=1, distance_sample=1)
 
 
 def test_plot_distance_scatterplot(catd_analysis):
@@ -94,22 +187,5 @@ def test_plot_distance_scatterplot(catd_analysis):
     )
 
 
-def test_plot_distance_misclasifed(catd_analysis):
-    catd_analysis.plot_distance_misclasified("lin", "goodall2", distance_sample=1)
-
-
-def test_array_of_distances(catd_analysis):
-    dpoint1 = catd_analysis.get_datapoint(
-        rd.randint(0, catd_analysis.get_data_len() - 1)
-    )
-    array1 = catd_analysis.array_of_distance(dpoint1, "lin", 10)
-    array2 = catd_analysis.array_of_distance(dpoint1, "lin", 10)
-    assert array1 == array2
-
-
-def test_knearest_points(catd_analysis):
-    k = 5
-    dpoint1 = catd_analysis.get_datapoint(
-        rd.randint(0, catd_analysis.get_data_len() - 1)
-    )
-    catd_analysis.plot_knearest_points(dpoint1, "goodall3", "overlap", "goodall2", k)
+def test_plot_distance_misclassifed(catd_analysis):
+    catd_analysis.plot_distance_misclassified("lin", "goodall2", distance_sample=1)
