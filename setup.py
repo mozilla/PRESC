a181b905a06c4bc811d99be32ad91c7980c19efa
Dave Zeber <dzeber@mozilla.com>
2021-03-12 16:38:40 -0600

    Update setup.py for distribution

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import find_packages, setup
 
+VERSION = "0.1.2"
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    # Keep only the top part until the first section break.
+    # (temporary workaround)
+    readme = fh.read()
+    long_description = readme[: readme.find("\n\n## ")]
 
 setup(
     name="presc",
     use_scm_version=False,
-    version="0.1.1",
+    version=VERSION,
     setup_requires=["setuptools_scm", "pytest-runner"],
     tests_require=["pytest"],
     include_package_data=True,
@@ -16,18 +20,31 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Mozilla Corporation",
-    author_email="mroviraesteva@mozilla.com",
-    url="https://github.com/mozilla/presc",
+    author_email="dzeber@mozilla.com",
+    url="https://github.com/mozilla/PRESC",
     license="MPL 2.0",
-    install_requires=[],
+    python_requires=">=3.7",
+    install_requires=[
+        "confuse",
+        "numpy",
+        "pandas>=1.0.0",
+        "scikit-learn>=0.23.1",
+        "scipy",
+        "matplotlib",
+        "seaborn",
+        "pyyaml",
+        "jupyter-book>=0.9.1",
+    ],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment :: Mozilla",
         "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Scientific/Engineering :: Information Analysis",
     ],
     zip_safe=False,
