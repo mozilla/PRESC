2c87dd7f55169db8edada966f20c7503cbdf1bf4
Dave Zeber <dzeber@mozilla.com>
2021-03-12 21:26:02 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "0.1.2"
+VERSION = "0.2.0"
 
 with open("README.md", "r") as fh:
     # Keep only the top part until the first section break.
