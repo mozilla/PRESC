b4cb9ffccc613475a2c48f745e406c3c06829847
Dave Zeber <dzeber@mozilla.com>
2021-06-11 19:30:31 -0500

    Refactor train_test_dataset

--- presc/deprecated/train_test_dataset.py
+++ presc/deprecated/train_test_dataset.py
@@ -1,7 +1,13 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from presc.dataset import Dataset
 
 
+# --------------------
+# This is no longer needed.
+# It has been replaced by presc.dataset.Dataset
+# Keeping here so as not to break the tests.
+# --------------------
 class TrainTestDataset:
     """Dataset API
 
@@ -15,87 +21,67 @@ class TrainTestDataset:
     """
 
     def __init__(self, df: pd.DataFrame, label: str) -> None:
-        self._dataset = df
-        self._X = None
-        self._y = None
-        self._X_train = None
-        self._X_test = None
-        self._y_train = None
-        self._y_test = None
-
-        self.set_label(label)
-
-    def set_label(self, label: str) -> None:
-        """This function allows users to reset the label column and will also update the feature columns.
-
-        Make sure you re-split the test and train data if you called this function after calling split.
-
-        Args:
-            label (string): name of the label column
-        """
-        try:
-            self._X = self._dataset.drop(columns=[label])
-            self._y = self._dataset[label]
-        except KeyError:
-            print(
-                "Please make sure that the label you speficy is in the columns:"
-                + ", ".join(self._dataset.columns.tolist())
-            )
-            raise
+        self._dataset = Dataset(df, label)
+        self._train = None
+        self._test = None
 
     def split_test_train(self, test_size: float = 0.2, random_state: int = 0) -> None:
-        self._X_train, self._X_test, self._y_train, self._y_test = train_test_split(
-            self._X, self._y, test_size=test_size, random_state=random_state
+        X_train, X_test, y_train, y_test = train_test_split(
+            self._dataset.features,
+            self._dataset.labels,
+            test_size=test_size,
+            random_state=random_state,
         )
+        df_train = pd.DataFrame(X_train, columns=self._dataset.feature_names)
+        df_train[self._dataset._label_col] = y_train
+        self._train = Dataset(df_train, self._dataset._label_col)
+
+        df_test = pd.DataFrame(X_test, columns=self._dataset.feature_names)
+        df_test[self._dataset._label_col] = y_test
+        self._test = Dataset(df_test, self._dataset._label_col)
 
     @property
     def raw_dataset(self) -> pd.DataFrame:
         """Returns the underlying dataset."""
-        return self._dataset
+        return self._dataset.df
 
     @property
     def features(self) -> pd.DataFrame:
         """Returns the full dataset feature columns."""
-        return self._X
+        return self._dataset.features
 
     @property
     def feature_names(self) -> list:
         """Returns the feature names as a list."""
-        return list(self.features.columns)
+        return self._dataset.feature_names
 
     @property
     def labels(self) -> pd.Series:
         """Returns the full dataset label column."""
-        return self._y
+        return self._dataset.labels
 
     @property
     def train_features(self) -> pd.DataFrame:
         """Returns the feature columns for the training set portion."""
-        return self._X_train
+        return self._train.features
 
     @property
     def train_labels(self) -> pd.Series:
         """Returns the label column for the training set portion."""
-        return self._y_train
+        return self._train.labels
 
     @property
     def test_features(self) -> pd.DataFrame:
         """Returns the feature columns for the test set portion."""
-        return self._X_test
+        return self._test.features
 
     @property
     def test_labels(self) -> pd.Series:
         """Returns the label column for the test set portion."""
-        return self._y_test
+        return self._test.labels
 
     def get_test_dataset(self) -> pd.DataFrame:
-        dataset_test = self._X_test.merge(
-            self._y_test, left_index=True, right_index=True, how="left"
-        )
-        return dataset_test
+        return self._test.df
 
     def get_train_dataset(self) -> pd.DataFrame:
-        dataset_train = self._X_train.merge(
-            self._y_train, left_index=True, right_index=True, how="left"
-        )
-        return dataset_train
+        return self._train.df
