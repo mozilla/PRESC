4f7014a944b708b556e6a99e7a41c24dae261f9c
alberginia <muriel.pirata@gmail.com>
2021-08-11 03:26:26 +0200

    Remove sampling function positional argumentss from the copy class.

--- presc/copies/copying.py
+++ presc/copies/copying.py
@@ -3,25 +3,15 @@ from presc.copies.evaluations import empirical_fidelity_error
 
 
 class ClassifierCopy:
-    def __init__(
-        self,
-        original,
-        copy,
-        sampling_function,
-        *sampling_parameters,
-        **k_sampling_parameters
-    ):
+    def __init__(self, original, copy, sampling_function, **k_sampling_parameters):
         self.original = original
         self.copy = copy
         self.sampling_function = sampling_function
-        self.sampling_parameters = sampling_parameters
         self.k_sampling_parameters = k_sampling_parameters
 
     def copy_classifier(self):
         # Generate synthetic data
-        X_generated = self.sampling_function(
-            *self.sampling_parameters, **self.k_sampling_parameters
-        )
+        X_generated = self.sampling_function(**self.k_sampling_parameters)
         df_generated = labeling(X_generated, self.original)
 
         # Copy the classifier
@@ -29,9 +19,7 @@ class ClassifierCopy:
 
     def get_training_data(self, label_col="y"):
         # Random state needs to be fixed to obtain the same training data
-        X_generated = self.sampling_function(
-            *self.sampling_parameters, **self.k_sampling_parameters
-        )
+        X_generated = self.sampling_function(**self.k_sampling_parameters)
         df_generated = labeling(X_generated, self.original, label_col=label_col)
         return df_generated
 
@@ -42,9 +30,7 @@ class ClassifierCopy:
         k_sampling_parameters_gen["random_state"] = random_state
         k_sampling_parameters_gen["nsamples"] = generated_nsamples
 
-        X_generated = self.sampling_function(
-            *self.sampling_parameters, **k_sampling_parameters_gen
-        )
+        X_generated = self.sampling_function(**k_sampling_parameters_gen)
         df_generated = labeling(X_generated, self.original, label_col=label_col)
         return df_generated
 
