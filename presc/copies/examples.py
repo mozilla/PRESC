54fba741298326b5cb309a065bf53e1f006b4e54
alberginia <muriel.pirata@gmail.com>
2021-08-27 03:31:26 +0200

    Gaussian dataset generator code cleanup and readability.

--- presc/copies/examples.py
+++ presc/copies/examples.py
@@ -7,7 +7,7 @@ from presc.dataset import Dataset
 
 
 def multiclass_gaussians(
-    nsamples=2000,
+    nsamples=3000,
     nfeatures=30,
     nclasses=15,
     center_low=2,
@@ -20,13 +20,15 @@ def multiclass_gaussians(
     This function generates a multidimensional normal distribution centered at
     the origin with standard deviation one for class zero. And then adds an
     additional gaussian distribution per class, centered at a random distance
-    beetween `center_low` and `center_high`, and with random standard deviation
+    between `center_low` and `center_high`, and with random standard deviation
     between `scale_low` and `scale_high`.
 
     Parameters
     ----------
     nsamples : int
-        Number of samples to generate per class.
+        Maximum number of samples to generate. Actual number of samples depends
+        on the number of classes, because the function yields a balanced
+        dataset with the same number of samples per class.
     nfeatures : int
         Number of features of the generated samples.
     nclasses : int
@@ -49,38 +51,34 @@ def multiclass_gaussians(
     presc.dataset.Dataset
         Outputs a PRESC Dataset with the generated samples and their labels.
     """
-    # t_pred=np.random.normal(15, math.sqrt(15), (1000,20))
+    class_samples = int(nsamples / nclasses)
 
     # Create class zero drawing samples from a `nfeatures`-dimensional normal
     # distribution centered at the origin and with standard deviation one.
-    t_pred = np.random.normal(0, 1, (nsamples, nfeatures))
+    t_pred = np.random.normal(0, 1, (class_samples, nfeatures))
     df_pred = pd.DataFrame(t_pred)
     df_pred["class"] = 0
 
     # Create additional classes centered at `m` with standard deviation `scale`
-    if nclasses > 1:
-        for i in range(1, nclasses):
-            # Generate a normalized vector in a random direction
-            v = np.random.multivariate_normal(
-                np.zeros((nfeatures,)), np.eye(nfeatures, nfeatures)
-            )
-            v = v[np.newaxis, :] / np.linalg.norm(v)
+    for i in range(1, nclasses):
+        # Generate a normalized vector in a random direction
+        v = np.random.normal(0, 1, nfeatures)
+        v = v / np.linalg.norm(v)
 
-            # Generate a random distance from the origin to define the center of each gaussian
-            alpha = np.random.uniform(low=center_low, high=center_high)
-            m = np.dot(alpha, v)
+        # Generate a random distance from the origin to define the center of each gaussian
+        alpha = np.random.uniform(low=center_low, high=center_high)
+        m = alpha * v
 
-            # Generate a random scaling for each gaussian
-            scale = np.random.uniform(low=scale_low, high=scale_high)
+        # Generate a random scaling for each gaussian
+        scale = np.random.uniform(low=scale_low, high=scale_high)
 
-            # Generate normally distributed random samples for this classs
-            t = m + scale * np.random.normal(0, 1, (nsamples, nfeatures))
-            df = pd.DataFrame(t)
-            df["class"] = i
+        # Generate normally distributed random samples for this classs
+        t = m + scale * np.random.normal(0, 1, (class_samples, nfeatures))
+        df = pd.DataFrame(t)
+        df["class"] = i
 
-            # Add class data to the dataset
-            df_c = pd.concat([df_pred, df], ignore_index=True)
-            df_pred = df_c
+        # Add class data to the dataset
+        df_pred = pd.concat([df_pred, df], ignore_index=True)
 
     # Convert into PRESC Dataset
     df_presc = Dataset(df_pred, label_col="class")
