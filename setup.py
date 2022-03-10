934d01dd0b93a0ca4887a3ea444edbbcb2c66c46
Dave Zeber <dzeber@mozilla.com>
2022-03-10 12:35:33 -0600

    Update environment packages & python version

--- setup.py
+++ setup.py
@@ -44,6 +44,8 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Scientific/Engineering :: Information Analysis",
     ],
