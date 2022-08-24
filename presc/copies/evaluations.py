6de2e43a361343ff1c757aba5c9287b73e8cc38e
alberginia <muriel.pirata@gmail.com>
2022-08-24 18:32:06 +0200

    Add titles parameter in multivariable_density_comparison function.

--- presc/copies/evaluations.py
+++ presc/copies/evaluations.py
@@ -156,13 +156,7 @@ def multivariable_density_comparison(
     feature2=None,
     label_col="class",
     titles=None,
-    other_kwargs={
-        "alpha": 0.3,
-        "common_norm": False,
-        "fill": True,
-        "n_levels": 4,
-        "legend": False,
-    },
+    other_kwargs=None,
 ):
     """Visualization to compare class density projections in detail.
 
@@ -184,8 +178,7 @@ def multivariable_density_comparison(
     Parameters
     ----------
     datasets : list of pandas DataFrames
-        List of the datasets with the sampled and labeled points for each
-        classifier included in the comparison.
+        List of the datasets with the sampled and labeled points for each classifier included in the comparison.
     feature1 :
         Name of feature to display in the x-axis.
     feature2 :
@@ -195,103 +188,36 @@ def multivariable_density_comparison(
     titles : list of str
         List of names to identify each classifier and label their subplot.
     **other_kwargs : dict
-        Any other seaborn.kdeplot parameters needed to adjust the visualization.
-        Default parameters are {"alpha": 0.3, "common_norm": False, "fill": True,
-        "n_levels": 4, "legend": False}. The value of any parameter specified
-        within the other_kwargs dictionary will be overwritten, including any
-        of these.
+        Any other parameters needed for the plotting function.
 
     Returns
     -------
     matplotlib.figure.Figure
         Figure with the detailed classifier comparison.
     matplotlib.axes.Axes or array of Axes
-        Contains most of the figure elements of the classifier comparison and
-        sets the coordinate system.
+        Contains most of the figure elements of the classifier comparison and sets the coordinate system.
     """
-    kdeplot_kwargs = {
-        "alpha": 0.3,
-        "common_norm": False,
-        "fill": True,
-        "n_levels": 4,
-        "legend": False,
-    }
-    for key, value in other_kwargs.items():
-        kdeplot_kwargs[key] = value
-
     num_comparisons = len(datasets)
-    class_names = set()
-    for index_models in range(num_comparisons):
-        class_names = class_names | set(datasets[index_models][label_col].unique())
-    class_names = sorted(list(class_names))
-    max_num_classes = len(class_names)
-
+    num_classes = datasets[0][label_col].nunique()
     fig, axs = plt.subplots(
-        max_num_classes,
-        num_comparisons,
-        figsize=(3.5 * num_comparisons, 3.5 * max_num_classes),
-        sharex=True,
-        sharey=True,
+        num_classes, len(datasets), figsize=(16, 7), sharex=True, sharey=True
     )
-    for index_models in range(num_comparisons):
-        for index_classes, class_name in enumerate(class_names):
-            axs[index_classes, index_models] = sns.kdeplot(
-                x=datasets[index_models][
-                    datasets[index_models][label_col] == class_name
+    for index_vertical in range(num_comparisons):
+        for index_horizontal in range(num_classes):
+            axs[index_horizontal, index_vertical] = sns.kdeplot(
+                x=datasets[index_vertical][
+                    datasets[index_vertical][label_col] == index_horizontal
                 ][feature1],
-                y=datasets[index_models][
-                    datasets[index_models][label_col] == class_name
+                y=datasets[index_vertical][
+                    datasets[index_vertical][label_col] == index_horizontal
                 ][feature2],
-                hue=datasets[index_models][label_col],
-                ax=axs[index_classes, index_models],
-                **kdeplot_kwargs,
-            )
-            axs[index_classes, 0].set_ylabel(
-                label_col + " = " + str(class_name) + "\n\n" + feature2
+                hue=datasets[index_vertical][label_col],
+                ax=axs[index_horizontal, index_vertical],
+                **other_kwargs,
             )
         if titles is not None:
-            axs[0, index_models].set_title(titles[index_models])
-    plt.show(block=False)
+            axs[0, index_vertical].set_title(titles[index_vertical])
+    plt.show()
 
     return fig, axs
 
-
-def keep_top_classes(dataset, min_num_samples=2, classes_to_keep=None):
-    """Function to remove rows from minoritary classes from PRESC Datasets.
-
-    Only classes that have more than the specified minimum number of samples
-    will be kept. If a list of the classes of interest is indicated, then this
-    requirement is overrided.
-
-    Parameters
-    ----------
-    dataset : presc.dataset.Dataset
-        PRESC dataset from which we want to remove the minoritary classes.
-    min_num_samples : int
-        Minimum number of samples that the classes should have in order to keep
-        them.
-    classes_to_keep : list
-        Name of the classes to keep. If a list of classes is specified here,
-        then the parameter `min_num_samples` is overriden, and the specified
-        classes will have any number of samples.
-
-    Returns
-    -------
-    presc.dataset.Dataset
-        PRESC Dataset without the samples from the minoritary classes.
-    """
-    label_col = dataset.labels.name
-    if classes_to_keep is None:
-        classes_to_keep = (
-            dataset.df[label_col]
-            .value_counts()[dataset.df[label_col].value_counts() >= min_num_samples]
-            .index.to_list()
-        )
-    new_dataframe = dataset.df[dataset.df[label_col].isin(classes_to_keep)].copy()
-    if new_dataframe[label_col].dtype.name == "categorical":
-        print(str(label_col), " is categorical")
-        new_dataframe[label_col] = new_dataframe.loc[:, label_col].cat.set_categories(
-            classes_to_keep
-        )
-
-    return Dataset(new_dataframe, label_col=label_col)
