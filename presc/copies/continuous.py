986d0eb849076ea1948cf380ee23c64d4ef212ae
alberginia <muriel.pirata@gmail.com>
2022-09-07 18:26:22 +0200

    Add print comment in ContinuousCopy class.

--- presc/copies/continuous.py
+++ presc/copies/continuous.py
@@ -169,6 +169,80 @@ class ContinuousCopy(Thread):
             pass
 
 
+class ContinuousCopy(Thread):
+    """Continuous classifier copy for online classifiers.
+
+    With this class for online classifiers partial fits of the copy can be
+    carried out sequentially.
+    """
+
+    def __init__(
+        self, classifier_copy, data_stream, fit_kwargs={}, verbose=False, test_data=None
+    ):
+        Thread.__init__(self)
+        self.classifier_copy = classifier_copy
+        self.data_stream = data_stream
+        self.fit_kwargs = fit_kwargs
+        self.verbose = verbose
+        self.test_data = test_data
+
+        self.iterations = 0
+        self.n_samples = 0
+
+        if isinstance(self.classifier_copy.copy, Pipeline):
+            self.is_pipeline = True
+        else:
+            self.is_pipeline = False
+
+    def run(self):
+        self.continuous_copy_run = True
+
+        self.partial_fit_ok = check_partial_fit(self.classifier_copy.copy)
+        if not self.partial_fit_ok:
+            print("Sorry, not all those transformers/estimators have 'partial_fit'.")
+
+        while self.continuous_copy_run and self.partial_fit_ok:
+            # Get data
+            data_block = self.data_stream.get()
+            n_samples_block = len(data_block.df)
+
+            # If pipeline, train elements of pipeline sequentially,
+            # and use transformed data to fit the next transformer or estimator
+            if self.is_pipeline:
+                n_estimators = len(self.classifier_copy.copy.named_steps)
+                X = data_block.features.copy()
+                for index, element in enumerate(self.classifier_copy.copy.named_steps):
+                    self.classifier_copy.copy[element].partial_fit(
+                        X, y=data_block.labels, **self.fit_kwargs[element]
+                    )
+                    # Transform data for next estimator except for the last one
+                    if index + 1 < n_estimators:
+                        X = self.classifier_copy.copy.named_steps[element].transform(X)
+
+            # If single classifier, train model with this block
+            else:
+                self.classifier_copy.copy.partial_fit(
+                    data_block.features, data_block.labels, **self.fit_kwargs
+                )
+            self.iterations += 1
+            self.n_samples += n_samples_block
+
+            if self.verbose:
+                print("\nIteration: ", self.iterations)
+                print("Samples: ", self.n_samples, "\n")
+
+                self.classifier_copy.evaluation_summary(
+                    test_data=self.test_data, synthetic_data=data_block
+                )
+
+    def stop(self):
+        self.continuous_copy_run = False
+        if self.verbose:
+            print("Stopping online classifier copier...\n")
+            print(f"The classifier copy trained for {self.iterations} iterations")
+            print(f"with a total of {self.n_samples:,} samples.\n".replace(",", "."))
+
+
 def check_partial_fit(estimator_pipeline):
     """Function to check if estimator or pipeline have partial_fit.
 
