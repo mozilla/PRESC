44751d21e420141fbea04836ba37a6033db47337
alberginia <muriel.pirata@gmail.com>
2022-09-21 07:18:32 +0200

    Add example black box model for the ADS mixed dataset.

--- examples/ML_classifier_copies/ML_copies_original_models.py
+++ examples/ML_classifier_copies/ML_copies_original_models.py
@@ -1,3 +1,4 @@
+import catboost
 import io
 import requests
 import pandas as pd
@@ -11,7 +12,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 
 from presc.dataset import Dataset
-from presc.copies.sampling import dynamical_range
+from presc.copies.sampling import dynamical_range, mixed_data_features
 
 
 class WinesModel:
@@ -147,3 +148,117 @@ class SegmentationModel:
 
         # SVC Model
         self.model = self.model.fit(self.X_train, self.y_train)
+
+
+class AutismScreeningModel:
+    """Classifier model for the Image Segmentation dataset."""
+
+    def __init__(self):
+        # Obtain and preprocess dataset
+        url = (
+            "https://archive.ics.uci.edu/ml/machine-learning-databases/00426/"
+            "Autism-Adult-Data%20Plus%20Description%20File.zip"
+        )
+        compressed_file = ZipFile(io.BytesIO(requests.get(url).content))
+        feature_names = [
+            "A1_Score",
+            "A2_Score",
+            "A3_Score",
+            "A4_Score",
+            "A5_Score",
+            "A6_Score",
+            "A7_Score",
+            "A8_Score",
+            "A9_Score",
+            "A10_Score",
+            "age",
+            "gender",
+            "ethnicity",
+            "jundice",
+            "autism",
+            "country_of_res",
+            "used_app_before",
+            "result",
+            "age_desc",
+            "relation",
+            "ASD",
+        ]
+        dtype = {
+            "age": "Int64",
+            "gender": "category",
+            "jundice": "category",
+            "autism": "category",
+            "A1_Score": "category",
+            "A2_Score": "category",
+            "A3_Score": "category",
+            "A4_Score": "category",
+            "A5_Score": "category",
+            "A6_Score": "category",
+            "A7_Score": "category",
+            "A8_Score": "category",
+            "A9_Score": "category",
+            "A10_Score": "category",
+            "ASD": "category",
+        }
+        dataset = pd.read_csv(
+            compressed_file.open("Autism-Adult-Data.arff"),
+            skiprows=25,
+            names=feature_names,
+            na_values="?",
+            dtype=dtype,
+        )
+        dataset = dataset.drop(
+            columns=[
+                "ethnicity",
+                "country_of_res",
+                "used_app_before",
+                "result",
+                "age_desc",
+                "relation",
+            ]
+        )
+        dataset = dataset[
+            ["age", "gender", "jundice", "autism"]
+            + dataset.columns[:10].to_list()
+            + ["ASD"]
+        ]
+        dataset = dataset.dropna()
+        dataset["age"] = dataset["age"].astype("int64")
+        dataset["ASD"] = dataset["ASD"].cat.rename_categories(
+            new_categories={"NO": 0, "YES": 1}
+        )
+        self.dataset = Dataset(dataset, label_col="ASD")
+
+        # Obtain feature space description
+        self.feature_description = mixed_data_features(self.dataset.features)
+        self.feature_description["age"]["max"] = 100
+
+        # Split dataset in test and train samples
+        X = self.dataset.features
+        y = self.dataset.labels
+        (self.X_train, self.X_test, self.y_train, self.y_test) = train_test_split(
+            X, y, test_size=0.20, random_state=0, stratify=y
+        )
+
+        categorical_features = [
+            "gender",
+            "jundice",
+            "autism",
+            "A1_Score",
+            "A2_Score",
+            "A3_Score",
+            "A4_Score",
+            "A5_Score",
+            "A6_Score",
+            "A7_Score",
+            "A8_Score",
+            "A9_Score",
+            "A10_Score",
+        ]
+        train_pool = catboost.Pool(
+            self.X_train, self.y_train, cat_features=categorical_features
+        )
+
+        # Gradient Boosting on Decision Trees Model
+        self.model = catboost.CatBoostClassifier()
+        self.model.fit(train_pool, verbose=False)
